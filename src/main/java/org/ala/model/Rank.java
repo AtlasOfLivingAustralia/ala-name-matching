b65653bcd9427139248758e56f2956a86c96bd34
nickdos <nickdos@gmail.com>
2009-03-19 10:26:43 +0000

    Added Rank class (not working) will remove in next commit.
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/ala-portal-web@311 1ef1b718-b4f5-11dd-90a5-ed332e521da9

---
+++ src/main/java/org/ala/model/Rank.java
@@ -0,0 +1,64 @@
+/***************************************************************************
+ * Copyright (C) 2009 Atlas of Living Australia
+ * All Rights Reserved.
+ *
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ ***************************************************************************/
+package org.ala.model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.hibernate.search.annotations.Field;
+import org.hibernate.search.annotations.Index;
+import org.hibernate.search.annotations.Indexed;
+import org.hibernate.search.annotations.Store;
+
+@Entity
+@Indexed(index="Ranks")
+@javax.persistence.Table(name="rank", schema="portal")
+public class Rank implements Serializable {
+
+    private static final long serialVersionUID = -7405551279255773047L;
+
+	@Id
+	@Field(index=Index.NO, store=Store.YES)
+	protected int id;
+
+    @Field(index=Index.UN_TOKENIZED, store=Store.YES)
+	protected String name;
+	
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+}
