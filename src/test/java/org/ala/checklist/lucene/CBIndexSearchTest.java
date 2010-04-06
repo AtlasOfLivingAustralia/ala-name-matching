4838c91f0e7b5daf244d8e97924b359ce8d11e30
peter.flemming@csiro.au <peter.flemming@csiro.au>
2010-04-06 05:53:56 +0000

    Modified to use constructor instead of init().
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/ala-name-matching@679 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/test/java/org/ala/checklist/lucene/CBIndexSearchTest.java
+++ src/test/java/org/ala/checklist/lucene/CBIndexSearchTest.java
@@ -12,64 +12,71 @@ import org.ala.checklist.lucene.model.NameSearchResult;
 public class CBIndexSearchTest {
     CBIndexSearch searcher;
 
-     @org.junit.Before
-    public void init() {
-        try {
-            searcher = new CBIndexSearch();
-            searcher.init("/data/lucene/cb/classification");
-        }
-        catch(Exception e){
+	@org.junit.Before
+	public void init() {
+		try {
+			searcher = new CBIndexSearch("/data/lucene/cb/classification");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-        }
+	}
 
-    }
-     @org.junit.Test
-     public void testNoRank(){
-         try{
-         String lsid = searcher.searchForLSID("Animalia");
-         System.out.println(lsid);
-         lsid = searcher.searchForLSID("Bacteria");
-         System.out.println(lsid);
-         }
-         catch(SearchResultException e){}
-     }
-     @org.junit.Test
-     public void testSpecies(){
-         try{
-         NameSearchResult nsr = searcher.searchForRecord("Holconia nigrigularis", "7000");
-         System.out.println(nsr);
-         }
-         catch(SearchResultException e){}
-     }
-     private void printAllResults(String prefix, List<NameSearchResult> results){
-         System.out.println("## "+prefix+" ##");
-         for(NameSearchResult result : results)
-             System.out.println(result);
-         System.out.println("###################################");
-     }
-     @org.junit.Test
-     public void testHomonym(){
-         try{
-             
-             List<NameSearchResult> results =searcher.searchForRecords("Simsia", "6000", "Animalia",  "Simsia", 10);
-             printAllResults("hymonyms test 1", results);
+	@org.junit.Test
+	public void testNoRank() {
+		try {
+			String lsid = searcher.searchForLSID("Animalia");
+			System.out.println(lsid);
+			lsid = searcher.searchForLSID("Bacteria");
+			System.out.println(lsid);
+		} catch (SearchResultException e) {
+			e.printStackTrace();
+		}
+	}
 
-             results =searcher.searchForRecords("Simsia", "6000", "Anmalia",  "Simsia", 10);
-             printAllResults("hymonyms test (Anmalia)", results);
-             
-             results = searcher.searchForRecords("Simsia", "6000", null, "Simsia", 10);
-             printAllResults("homonyms test 2", results);
-             
-         }
-         catch(SearchResultException e){
-             System.err.println(e.getMessage());
-             printAllResults("HOMONYM EXCEPTION", e.getResults());
-             
-         }
-     }
-     @org.junit.Test
-     public void testIDLookup(){
-         NameSearchResult result = searcher.searchForRecordByID("216346");
-         System.out.println(result);
-     }
+	@org.junit.Test
+	public void testSpecies() {
+		try {
+			NameSearchResult nsr = searcher.searchForRecord(
+					"Holconia nigrigularis", "7000");
+			System.out.println(nsr);
+		} catch (SearchResultException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void printAllResults(String prefix, List<NameSearchResult> results) {
+		System.out.println("## " + prefix + " ##");
+		for (NameSearchResult result : results)
+			System.out.println(result);
+		System.out.println("###################################");
+	}
+
+	@org.junit.Test
+	public void testHomonym() {
+		try {
+
+			List<NameSearchResult> results = searcher.searchForRecords(
+					"Simsia", "6000", "Animalia", "Simsia", 10);
+			printAllResults("hymonyms test 1", results);
+
+			results = searcher.searchForRecords("Simsia", "6000", "Anmalia",
+					"Simsia", 10);
+			printAllResults("hymonyms test (Anmalia)", results);
+
+			results = searcher.searchForRecords("Simsia", "6000", null,
+					"Simsia", 10);
+			printAllResults("homonyms test 2", results);
+
+		} catch (SearchResultException e) {
+			System.err.println(e.getMessage());
+			printAllResults("HOMONYM EXCEPTION", e.getResults());
+		}
+	}
+
+	@org.junit.Test
+	public void testIDLookup() {
+		NameSearchResult result = searcher.searchForRecordByID("216346");
+		System.out.println(result);
+	}
 }
