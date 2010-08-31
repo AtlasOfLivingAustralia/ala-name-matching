83b643e487c1fa7a639cab94f5b8263daf51ace1
peter.flemming@csiro.au <peter.flemming@csiro.au>
2010-08-31 08:25:16 +0000

    Replaced by SensitiveSpeciesDao
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/sensitive-species@1148 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/java/au/org/ala/sensitiveData/dao/LookupDao.java
+++
@@ -1,9 +0,0 @@
-package au.org.ala.sensitiveData.dao;
-
-import au.org.ala.sensitiveData.model.SensitiveSpecies;
-
-public interface LookupDao {
-
-	SensitiveSpecies findByName(String scientificName);
-	
-}
