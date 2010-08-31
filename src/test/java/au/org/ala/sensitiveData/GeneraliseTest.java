b273c566fdde8368cf86cf13512ddae4b037271e
peter.flemming@csiro.au <peter.flemming@csiro.au>
2010-08-31 08:28:57 +0000

    Added javadoc
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/sensitive-species@1155 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/test/java/au/org/ala/sensitiveData/GeneraliseTest.java
+++ src/test/java/au/org/ala/sensitiveData/GeneraliseTest.java
@@ -1,3 +1,17 @@
+/***************************************************************************
+ * Copyright (C) 2010 Atlas of Living Australia
+ * All Rights Reserved.
+ *
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ ***************************************************************************/
 package au.org.ala.sensitiveData;
 
 import org.springframework.context.ApplicationContext;
@@ -7,15 +21,19 @@ import au.org.ala.sensitiveData.model.SensitiveSpecies;
 
 import junit.framework.TestCase;
 
+/**
+ *
+ * @author Peter Flemming (peter.flemming@csiro.au)
+ */
 public class GeneraliseTest extends TestCase {
 
 	ApplicationContext context;
-	SearchImpl finder;
+	SensitiveSpeciesFinder finder;
 	
 	protected void setUp() throws Exception {
 		super.setUp();
 		context = new ClassPathXmlApplicationContext("spring-config.xml");
-		finder = context.getBean("searchImpl", SearchImpl.class);
+		finder = context.getBean("searchImpl", SensitiveSpeciesFinder.class);
 	}
 
 	public void testGeneralisation() {
