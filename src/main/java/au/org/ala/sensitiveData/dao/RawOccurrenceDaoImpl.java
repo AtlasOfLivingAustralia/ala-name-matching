ce26664dbe49f5d455b05f40f85d615ede852297
peter.flemming@csiro.au <peter.flemming@csiro.au>
2010-08-31 08:25:42 +0000

    Added javadoc
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/sensitive-species@1149 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/java/au/org/ala/sensitiveData/dao/RawOccurrenceDaoImpl.java
+++ src/main/java/au/org/ala/sensitiveData/dao/RawOccurrenceDaoImpl.java
@@ -1,3 +1,17 @@
+/***************************************************************************
+ * Copyright (C) 2010 Atlas of Living Australia
+ * All Rights Reserved.
+ *
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ ***************************************************************************/
 package au.org.ala.sensitiveData.dao;
 
 import java.sql.ResultSet;
@@ -14,6 +28,10 @@ import org.springframework.stereotype.Component;
 
 import au.org.ala.sensitiveData.model.RawOccurrenceRecord;
 
+/**
+ *
+ * @author Peter Flemming (peter.flemming@csiro.au)
+ */
 @Component
 public class RawOccurrenceDaoImpl extends JdbcDaoSupport implements RawOccurrenceDao {
 	
