ec06f682b41ce5977a9446002124a4ddeb23c1b3
peter.flemming@csiro.au <peter.flemming@csiro.au>
2010-12-22 23:20:56 +0000

    Initial version
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/sensitive-species@1517 1ef1b718-b4f5-11dd-90a5-ed332e521da9

---
+++ src/main/java/au/org/ala/sensitiveData/ValidateSpecies.java
@@ -0,0 +1,15 @@
+package au.org.ala.sensitiveData;
+
+public class ValidateSpecies {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // Open file and validate metadata
+        
+        // 
+
+    }
+
+}
