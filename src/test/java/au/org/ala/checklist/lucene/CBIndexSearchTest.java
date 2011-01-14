bc664667b24cdf8379737cc71819ba2e90e98e33
tommywang1981@gmail.com <tommywang1981@gmail.com>
2011-01-14 00:50:35 +0000

    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/ala-name-matching@1579 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/test/java/au/org/ala/checklist/lucene/CBIndexSearchTest.java
+++ src/test/java/au/org/ala/checklist/lucene/CBIndexSearchTest.java
@@ -14,7 +14,7 @@ import au.org.ala.data.model.LinnaeanRankClassification;
  * @author Natasha
  */
 public class CBIndexSearchTest {
-    private static CBIndexSearch searcher;
+	private static CBIndexSearch searcher;
 
 	@org.junit.BeforeClass
 	public static void init() {
@@ -30,9 +30,9 @@ public class CBIndexSearchTest {
 	public void testNoRank() {
 		try {
 			String lsid = searcher.searchForLSID("Animalia");
-			System.out.println(lsid);
+			System.out.println("testNoRank: " + lsid);
 			lsid = searcher.searchForLSID("Bacteria");
-			System.out.println(lsid);
+			System.out.println("testNoRank: " + lsid);
 		} catch (SearchResultException e) {
 			e.printStackTrace();
 		}
@@ -55,39 +55,39 @@ public class CBIndexSearchTest {
 			System.out.println(result);
 		System.out.println("###################################");
 	}
-        @org.junit.Test
-        public void testSynonymWithHomonym(){
-            try{
-//               NameSearchResult result = searcher.searchForRecord("Macropus rufus", RankType.SPECIES);
-//               System.out.println("Macropus rufus: " + result);
-//               System.out.println("LSID: " + searcher.searchForLSID("Macropus rufus"));
-                LinnaeanRankClassification cl = new LinnaeanRankClassification("Animalia", "Atylus");
-                NameSearchResult result = searcher.searchForRecord("Atylus monoculoides", cl, RankType.SPECIES);
-                System.out.println("synonym: " + result);
-                System.out.println("LSID: " + searcher.searchForLSID("Atylus monoculoides"));
-                //System.out.println("LSID: " +searcher.searchForLSID("Sira tricincta"));
-            }
-            catch(Exception e){
-                //e.printStackTrace();//
-                if(e instanceof HomonymException){
-                    printAllResults("SYNONYM/HOMONYM: ", ((HomonymException)e).getResults());
-                }
-            }
-        }
+	@org.junit.Test
+	public void testSynonymWithHomonym(){
+		try{
+			//               NameSearchResult result = searcher.searchForRecord("Macropus rufus", RankType.SPECIES);
+			//               System.out.println("Macropus rufus: " + result);
+			//               System.out.println("LSID: " + searcher.searchForLSID("Macropus rufus"));
+			LinnaeanRankClassification cl = new LinnaeanRankClassification("Animalia", "Atylus");
+			NameSearchResult result = searcher.searchForRecord("Atylus monoculoides", cl, RankType.SPECIES);
+			System.out.println("synonym: " + result);
+			System.out.println("LSID: " + searcher.searchForLSID("Atylus monoculoides"));
+			//System.out.println("LSID: " +searcher.searchForLSID("Sira tricincta"));
+		}
+		catch(Exception e){
+			//e.printStackTrace();//
+			if(e instanceof HomonymException){
+				printAllResults("SYNONYM/HOMONYM: ", ((HomonymException)e).getResults());
+			}
+		}
+	}
 
 	@org.junit.Test
 	public void testHomonym() {
 		try {
 
-                        LinnaeanRankClassification cl = new LinnaeanRankClassification("Animalia", "Simsia");
+			LinnaeanRankClassification cl = new LinnaeanRankClassification("Animalia", "Simsia");
 			List<NameSearchResult> results = searcher.searchForRecords(
 					"Simsia", RankType.getForId(6000), cl, 10);
 			printAllResults("hymonyms test 1", results);
-                        //test to ensure that kingdoms that almost match are being will not report homonym exceptions
+			//test to ensure that kingdoms that almost match are being will not report homonym exceptions
 			cl.setKingdom("Anmalia");
-                        results = searcher.searchForRecords("Simsia", RankType.getForId(6000), cl, 10);
+			results = searcher.searchForRecords("Simsia", RankType.getForId(6000), cl, 10);
 			printAllResults("hymonyms test (Anmalia)", results);
-                        cl.setKingdom(null);
+			cl.setKingdom(null);
 			results = searcher.searchForRecords("Simsia", RankType.getForId(6000), cl, 10);
 			printAllResults("homonyms test 2", results);
 
@@ -102,120 +102,131 @@ public class CBIndexSearchTest {
 		NameSearchResult result = searcher.searchForRecordByID("216346");
 		System.out.println(result);
 	}
-	
+
 	@org.junit.Test
 	public void testSearchForRecord() {
 		NameSearchResult result = null;
 		try {
-                        LinnaeanRankClassification cl = new LinnaeanRankClassification(null, "Rhinotia");
+			LinnaeanRankClassification cl = new LinnaeanRankClassification(null, "Rhinotia");
 			result = searcher.searchForRecord(null, cl, null);
 		} catch (SearchResultException e) {
 			e.printStackTrace();
 		}
 		System.out.println(result);
 	}
-        @org.junit.Test
-        public void testCommonNames(){
-            //ANBG source
-            printCommonName("Red Kangaroo");
-            //COL source
-            printCommonName("Yellow-tailed Black-Cockatoo");
-            //not found
-            printCommonName("Scarlet Robin");
-            //CoL source that maps to a ANBG lsid
-            printCommonName("Australian tuna");
-            //ANBG and CoL have slightly different scientific name
-            printCommonName("Pacific Black Duck");
-            //Maps to many different species thus should return no LSID
-            printCommonName("Carp");
-        }
-        private void printCommonName(String name){
-            System.out.println(name + " " + searcher.searchForLSIDCommonName(name));
-        }
-        @org.junit.Test
-        public void testIRMNGHomonymReconcile(){
-            try{
-                LinnaeanRankClassification cl = new LinnaeanRankClassification("Animalia", "Chordata", null, null, "Macropodidae", "Macropus", null);
-                RankType rank = searcher.resolveIRMNGHomonym(cl);
-                System.out.println("IRMNG Homonym resolved at " + rank + " rank");
-                //now cause a homonym exception by removing the family
-                cl.setFamily(null);
-                searcher.resolveIRMNGHomonym(cl);
-            }
-            catch(HomonymException e){
-                System.out.println(e.getMessage());
-            }
-        }
-        @org.junit.Test
-        public void newHomonynmTest(){
-            try{
-                //Abelia grandiflora
-            }
-            catch(Exception e){
+	@org.junit.Test
+	public void testCommonNames(){
+		//ANBG source
+		printCommonName("Red Kangaroo");
+		//COL source
+		printCommonName("Yellow-tailed Black-Cockatoo");
+		//not found
+		printCommonName("Scarlet Robin");
+		//CoL source that maps to a ANBG lsid
+		printCommonName("Australian tuna");
+		//ANBG and CoL have slightly different scientific name
+		printCommonName("Pacific Black Duck");
+		//Maps to many different species thus should return no LSID
+		printCommonName("Carp");
+	}
+	private void printCommonName(String name){
+		System.out.println(name + " " + searcher.searchForLSIDCommonName(name));
+	}
+	@org.junit.Test
+	public void testIRMNGHomonymReconcile(){
+		try{
+			LinnaeanRankClassification cl = new LinnaeanRankClassification("Animalia", "Chordata", null, null, "Macropodidae", "Macropus", null);
+			RankType rank = searcher.resolveIRMNGHomonym(cl);
+			System.out.println("IRMNG Homonym resolved at " + rank + " rank");
+			//now cause a homonym exception by removing the family
+			cl.setFamily(null);
+			searcher.resolveIRMNGHomonym(cl);
+		}
+		catch(HomonymException e){
+			System.out.println(e.getMessage());
+		}
+	}
+	@org.junit.Test
+	public void newHomonynmTest(){
+		try{
+			//Abelia grandiflora
+		}
+		catch(Exception e){
 
-            }
-        }
-        @org.junit.Test
-        public void testCultivars(){
-            try{
-                //species level concept
-                System.out.println("Hypoestes phyllostachya: " +searcher.searchForLSID("Hypoestes phyllostachya"));
-                //cultivar level concept
-                System.out.println("Hypoestes phyllostachya 'Splash': " + searcher.searchForRecord("Hypoestes phyllostachya 'Splash'", null));
+		}
+	}
+	@org.junit.Test
+	public void testCultivars(){
+		try{
+			//species level concept
+			System.out.println("Hypoestes phyllostachya: " +searcher.searchForLSID("Hypoestes phyllostachya"));
+			//cultivar level concept
+			System.out.println("Hypoestes phyllostachya 'Splash': " + searcher.searchForRecord("Hypoestes phyllostachya 'Splash'", null));
 
-            }
-            catch(Exception e){
-                e.printStackTrace();
-            }
-        }
-        @org.junit.Test
-        public void testMyrmecia(){
-            try{
-                LinnaeanRankClassification cl = new LinnaeanRankClassification("Animalia","Arthropoda", "Insecta", "Hymenoptera", "Formicidae", "Myrmecia",null);
-                System.out.println(searcher.searchForRecord("Myrmecia", cl, null));
-            }
-            catch(Exception e){
-                e.printStackTrace();
-            }
-        }
-        @org.junit.Test
-        public void testFuzzyMatches(){
-            try{
-            //Bullia
-            System.out.println("Bullia NOT fuzzy: " + searcher.searchForRecord("Bullia", null));
-            System.out.println("Bullia fuzzy: " +searcher.searchForRecord("Bullia", null, true));
-            //Anochetus
-            LinnaeanRankClassification cl = new LinnaeanRankClassification("Animalia", "Arthropoda", "Insecta", "Hymenoptera", "Formicidae","Anochetus", null);
-            System.out.println("Anochetus NOT fuzzy: " + searcher.searchForLSID("Anochetus",cl, null));
-            System.out.println("Anochetus fuzzy: " + searcher.searchForLSID("Anochetus", null, true));
-            LinnaeanRankClassification classification = new LinnaeanRankClassification("Plantae", null, null, null, null, null, "Sauropus sp. A Kimberley Flora (T.E.H. Aplin et al. 929)");
-            System.out.println("Sauropus sp. A Kimberley Flora (T.E.H. Aplin et al. 929) : " + searcher.searchForLSID("Sauropus sp. A Kimberley Flora (T.E.H. Aplin et al. 929)", classification, null));
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	@org.junit.Test
+	public void testMyrmecia(){
+		try{
+			LinnaeanRankClassification cl = new LinnaeanRankClassification("Animalia","Arthropoda", "Insecta", "Hymenoptera", "Formicidae", "Myrmecia",null);
+			System.out.println(searcher.searchForRecord("Myrmecia", cl, null));
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	@org.junit.Test
+	public void testSearchForLSID(){
+		
+		try{
+			LinnaeanRankClassification cl = new LinnaeanRankClassification("Animalia","Arthropoda", "Insecta", "Hymenoptera", "Formicidae", "Myrmecia",null);
+			System.out.println(searcher.searchForRecord("Myrmecia", cl, null));
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	@org.junit.Test
+	public void testFuzzyMatches(){
+		try{
+			//Bullia
+			System.out.println("Bullia NOT fuzzy: " + searcher.searchForRecord("Bullia", null));
+			System.out.println("Bullia fuzzy: " +searcher.searchForRecord("Bullia", null, true));
+			//Anochetus
+			LinnaeanRankClassification cl = new LinnaeanRankClassification("Animalia", "Arthropoda", "Insecta", "Hymenoptera", "Formicidae","Anochetus", null);
+			System.out.println("Anochetus NOT fuzzy: " + searcher.searchForLSID("Anochetus",cl, null));
+			System.out.println("Anochetus fuzzy: " + searcher.searchForLSID("Anochetus", null, true));
+			LinnaeanRankClassification classification = new LinnaeanRankClassification("Plantae", null, null, null, null, null, "Sauropus sp. A Kimberley Flora (T.E.H. Aplin et al. 929)");
+			System.out.println("Sauropus sp. A Kimberley Flora (T.E.H. Aplin et al. 929) : " + searcher.searchForLSID("Sauropus sp. A Kimberley Flora (T.E.H. Aplin et al. 929)", classification, null));
 
-            }
-            catch(Exception e){
-                e.printStackTrace();
-            }
-        }
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
 
-        public void testWaiIssues(){
-//            System.out.println("#### TEST 1 ####"); synonym
-//            testName("Cyrtanthus elatus");
-//            testName("Cyrtanthus purpureus");
-//            System.out.println("#### TEST 2 ####"); synonym
-//            testName("Lycoris africana");
-//            testName("Lycoris aurea");
-//            System.out.println("#### TEST 3 ####");
-//            testName("Monstera deliciosa");
-//            testName("Monstera deliciosa 'Albo-Variegata'");
+	public void testWaiIssues(){
+		//            System.out.println("#### TEST 1 ####"); synonym
+		//            testName("Cyrtanthus elatus");
+		//            testName("Cyrtanthus purpureus");
+		//            System.out.println("#### TEST 2 ####"); synonym
+		//            testName("Lycoris africana");
+		//            testName("Lycoris aurea");
+		//            System.out.println("#### TEST 3 ####");
+		//            testName("Monstera deliciosa");
+		//            testName("Monstera deliciosa 'Albo-Variegata'");
 
-        }
-        public void testName(String name){
-            try{
-                System.out.println(name+": " + searcher.searchForRecord(name, null));
+	}
+	public void testName(String name){
+		try{
+			System.out.println(name+": " + searcher.searchForRecord(name, null));
 
-            }
-            catch(Exception e){
-                e.printStackTrace();
-            }
-        }
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
 }
