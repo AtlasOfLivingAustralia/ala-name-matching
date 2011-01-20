f68834305e5c88b6634011aaa5e6e0a6d293648e
peter.flemming@csiro.au <peter.flemming@csiro.au>
2011-01-20 06:31:44 +0000

    New dto's
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/sensitive-species@1630 1ef1b718-b4f5-11dd-90a5-ed332e521da9

---
+++ src/main/java/au/org/ala/sensitiveData/dto/SpeciesOccurrenceDto.java
@@ -0,0 +1,91 @@
+/***************************************************************************
+ * Copyright (C) 2010 Atlas of Living Australia
+ * All Rights Reserved.
+ *
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ ***************************************************************************/
+package au.org.ala.sensitiveData.dto;
+
+/**
+ *
+ * @author Peter Flemming (peter.flemming@csiro.au)
+ */
+public class SpeciesOccurrenceDto {
+	private int id;
+	private String scientificName;
+	private String location;
+	private String latitude;
+	private String longitude;
+	private String latLongPrecision;
+	private String basisOfRecord;
+	
+    public SpeciesOccurrenceDto() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getScientificName() {
+		return scientificName;
+	}
+
+	public void setScientificName(String scientificName) {
+		this.scientificName = scientificName;
+	}
+
+	public String getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+
+	public String getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+
+	public String getLatLongPrecision() {
+		return latLongPrecision;
+	}
+
+	public void setLatLongPrecision(String latLongPrecision) {
+		this.latLongPrecision = latLongPrecision;
+	}
+	
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getBasisOfRecord() {
+        return basisOfRecord;
+    }
+
+    public void setBasisOfRecord(String basisOfRecord) {
+        this.basisOfRecord = basisOfRecord;
+    }
+
+}
