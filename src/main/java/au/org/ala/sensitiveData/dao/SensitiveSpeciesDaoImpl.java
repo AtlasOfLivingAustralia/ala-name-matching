4edc2ddbd51fca43391d532e4a00e71844169fd8
peter.flemming@csiro.au <peter.flemming@csiro.au>
2011-01-20 06:31:02 +0000

    Changes to handle sensitive_species_location DB table
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/sensitive-species@1629 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/java/au/org/ala/sensitiveData/dao/SensitiveSpeciesDaoImpl.java
+++ src/main/java/au/org/ala/sensitiveData/dao/SensitiveSpeciesDaoImpl.java
@@ -16,6 +16,7 @@ package au.org.ala.sensitiveData.dao;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -26,8 +27,11 @@ import org.apache.log4j.Logger;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 
+import au.org.ala.sensitiveData.dto.SensitiveSpeciesDto;
 import au.org.ala.sensitiveData.model.SensitiveSpecies;
-import au.org.ala.sensitiveData.model.SensitivityCategory;
+import au.org.ala.sensitiveData.model.ConservationCategory;
+import au.org.ala.sensitiveData.model.SensitivityInstance;
+import au.org.ala.sensitiveData.model.SensitivityZone;
 
 /**
  *
@@ -39,7 +43,7 @@ public class SensitiveSpeciesDaoImpl extends JdbcDaoSupport implements Sensitive
 	
 	private List<SensitiveSpecies> species;
 
-	public static final String SELECT_ALL = "SELECT * FROM sensitive_species ORDER BY scientific_name";
+	public static final String SELECT_ALL = "SELECT * FROM sensitive_species_location ORDER BY scientific_name, sensitivity_zone";
 
 	@Inject
 	public SensitiveSpeciesDaoImpl(DataSource dataSource) {
@@ -53,18 +57,41 @@ public class SensitiveSpeciesDaoImpl extends JdbcDaoSupport implements Sensitive
 	}
 
 	public List<SensitiveSpecies> getAll() {
-		return (List<SensitiveSpecies>) getJdbcTemplate().query(
+
+	    List<SensitiveSpeciesDto> dtoList = getJdbcTemplate().query(
 				SELECT_ALL,
-				new RowMapper<SensitiveSpecies>() {
-					public SensitiveSpecies mapRow(ResultSet rs, int row) throws SQLException {
-						return new SensitiveSpecies(rs.getString("scientific_name"), SensitivityCategory.getCategory(rs.getString("sensitivity_category")));
+				new RowMapper<SensitiveSpeciesDto>() {
+					public SensitiveSpeciesDto mapRow(ResultSet rs, int row) throws SQLException {
+					    SensitiveSpeciesDto dto = new SensitiveSpeciesDto();
+					    dto.setScientificName(rs.getString("scientific_name"));
+					    dto.setSensitivityZone(rs.getString("sensitivity_zone"));
+					    dto.setDataProvider(rs.getString("data_provider"));
+					    dto.setSensitivityCategory(rs.getString("sensitivity_category"));
+						return dto;
 					}
 				});
+	    
+	    List<SensitiveSpecies> speciesList = new ArrayList<SensitiveSpecies>();
+	    SensitiveSpecies ss = null;;
+	    String currentName = "";
+
+	    for (SensitiveSpeciesDto dto : dtoList) {
+	        if (!dto.getScientificName().equals(currentName)) {
+	            ss = new SensitiveSpecies(dto.getScientificName());
+	            speciesList.add(ss);
+	        }
+	        ss.getInstances().add(new SensitivityInstance(
+	                ConservationCategory.getCategory(dto.getSensitivityCategory()), 
+	                dto.getDataProvider(), 
+	                SensitivityZone.valueOf(dto.getSensitivityZone())));
+	    }
+	    
+        return speciesList;
 	}
 
 	public SensitiveSpecies findByName(String scientificName) {
 		try {
-			SensitiveSpecies ss = new SensitiveSpecies(scientificName, null);
+			SensitiveSpecies ss = new SensitiveSpecies(scientificName);
 			int match = Collections.binarySearch(species, ss);
 			if (species.get(match).equals(ss)) {
 		        logger.debug("Sensitive Species exact match - " + scientificName);
