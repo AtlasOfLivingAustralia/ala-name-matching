44c1a59bf92e9e896256a106db69954a166fa09d
peter.flemming@csiro.au <peter.flemming@csiro.au>
2011-01-20 06:27:52 +0000

    Renamed to .../util/GeoLocationHelper
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/sensitive-species@1626 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/java/au/org/ala/sensitiveData/dao/GeoLocationDao.java
+++
@@ -1,7 +0,0 @@
-package au.org.ala.sensitiveData.dao;
-
-import au.org.ala.sensitiveData.model.SensitivityZone;
-
-public interface GeoLocationDao {
-    SensitivityZone getStateContainingPoint(String latitude, String longitude) throws Exception;
-}
