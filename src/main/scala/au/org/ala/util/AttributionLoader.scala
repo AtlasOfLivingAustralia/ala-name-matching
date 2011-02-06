1bae471e62894daf70e2daa177c5ce65144e8238
natasha.carter@csiro.au <natasha.carter@csiro.au>
2011-02-06 22:24:45 +0000

    Added data provider/resource UID and name to the AttributionLoader.
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/biocache-store@1776 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/scala/au/org/ala/util/AttributionLoader.scala
+++ src/main/scala/au/org/ala/util/AttributionLoader.scala
@@ -8,11 +8,18 @@ import org.wyki.cassandra.pelops.Pelops
  * Loads an export from the old portal database of point lookups.
  * 
  * This should be replaced with access to a webservice.
+ *
+ * At the moment the webservice does not return the data provider or data resource information
  * 
- * select ic.code, cc.code, icm.institution_uid, icm.collection_uid, ic.name from inst_coll_mapping icm
- * inner join institution_code ic ON ic.id = icm.institution_code_id
- * inner join collection_code cc ON cc.id = icm.collection_code_id
- * into outfile '/tmp/coll-mapping.txt';
+ * select ic.code, cc.code, icm.institution_uid, icm.collection_uid, ic.name, dp.uid, dp.name, dr.uid, dr.name from inst_coll_mapping icm
+  inner join institution_code ic ON ic.id = icm.institution_code_id
+  inner join collection_code cc ON cc.id = icm.collection_code_id
+  inner join occurrence_record oc on icm.institution_code_id = oc.institution_code_id and icm.collection_code_id = oc.collection_code_id
+  inner join data_resource dr on oc.data_resource_id = dr.id
+  inner join data_provider dp on oc.data_provider_id = dp.id
+  where dr.release_flag
+  group by ic.code, cc.code, icm.institution_uid, icm.collection_uid, ic.name,dp.uid, dp.name, dr.uid, dr.name
+  into outfile '/tmp/coll-mapping.txt';
  * 
  * @author Dave Martin (David.Martin@csiro.au)
  */
@@ -31,6 +38,10 @@ object AttributionLoader {
         attribution.institutionUid = parts(2)
         attribution.collectionUid = parts(3)
         attribution.institutionName = parts(4)
+        attribution.dataProviderUid = parts(5)
+        attribution.dataProviderName = parts(6)
+        attribution.dataResourceUid = parts(7)
+        attribution.dataResourceName = parts(8)
         AttributionDAO.add(parts(0), parts(1), attribution)
         if (counter % 1000 == 0) println(counter +": "+parts(0) +"|"+ parts(1))
       }
