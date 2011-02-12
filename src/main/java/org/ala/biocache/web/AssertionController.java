dead0228c172a596c05e061d969a4723e772c550
djtfmartin <djtfmartin@gmail.com>
2011-02-12 04:01:49 +0000

    additions for assertion API
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/biocache-service@1812 1ef1b718-b4f5-11dd-90a5-ed332e521da9

---
+++ src/main/java/org/ala/biocache/web/AssertionController.java
@@ -0,0 +1,76 @@
+package org.ala.biocache.web;
+
+import au.org.ala.biocache.ErrorCode;
+import au.org.ala.biocache.Store;
+import au.org.ala.biocache.QualityAssertion;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+@Controller
+public class AssertionController {
+
+    /**
+     * Retrieve an array of the assertion codes in use by the processing system
+     *
+     * @return an array of codes
+     * @throws Exception
+     */
+    @RequestMapping(value = {"/assertions/codes"}, method = RequestMethod.GET)
+	public @ResponseBody ErrorCode[] showCodes() throws Exception {
+        return Store.retrieveAssertionCodes();
+    }
+
+    /**
+     * add an assertion
+     */
+    @RequestMapping(value = {"/occurrences/{recordUuid}/assertions/add"}, method = RequestMethod.POST)
+	public void addAssertion(
+        @PathVariable(value="recordUuid") String recordUuid,
+        @RequestParam(value="code", required=true) Integer code,
+        @RequestParam(value="comment", required=false) String comment,
+        HttpServletRequest request,
+        HttpServletResponse response) throws Exception {
+        QualityAssertion qa = au.org.ala.biocache.QualityAssertion.apply(code);
+        Store.addUserAssertion(recordUuid, qa);
+        String server = request.getSession().getServletContext().getInitParameter("serverName");
+        response.setHeader("location", server + "/occurrence/" + recordUuid + "/assertions/" + qa.getUuid());
+        response.setStatus(HttpServletResponse.SC_CREATED);
+    }
+
+    /**
+     * Remove an assertion
+     */
+    @RequestMapping(value = {"/occurrences/{recordUuid}/assertions/delete"}, method = RequestMethod.POST)
+	public void deleteAssertion(
+        @PathVariable(value="recordUuid") String recordUuid,
+        @RequestParam(value="assertionUuid", required=true) String assertionUuid,
+        HttpServletResponse response) throws Exception {
+        Store.deleteUserAssertion(recordUuid, assertionUuid);
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+
+    /**
+     * Get single assertion
+     */
+    @RequestMapping(value = {"/occurrences/{recordUuid}/assertions/{assertionUuid}"}, method = RequestMethod.GET)
+	public @ResponseBody QualityAssertion getAssertion(
+        @PathVariable(value="recordUuid") String recordUuid,
+        @PathVariable(value="assertionUuid") String assertionUuid,
+        HttpServletResponse response) throws Exception {
+        QualityAssertion qa = Store.getUserAssertion(recordUuid, assertionUuid);
+        return qa;
+    }
+
+    /**
+     * Get assertions
+     */
+    @RequestMapping(value = {"/occurrences/{recordUuid}/assertions/"}, method = RequestMethod.GET)
+	public @ResponseBody List<QualityAssertion> getAssertions(
+        @PathVariable(value="recordUuid") String recordUuid) throws Exception {
+        return Store.getUserAssertions(recordUuid);
+    }
+}
