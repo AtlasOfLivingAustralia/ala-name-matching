2473153c2ff30d71cb49b929ca73e03715acbabd
peter.flemming@csiro.au <peter.flemming@csiro.au>
2011-02-18 06:52:10 +0000

    Added external territories
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/sensitive-species@1862 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/java/au/org/ala/sds/model/SensitivityZone.java
+++ src/main/java/au/org/ala/sds/model/SensitivityZone.java
@@ -9,7 +9,15 @@ public enum SensitivityZone {
     TAS("Tasmania"),
     SA("South Australia"),
     WA("Western Australia"),
-    NT("Northern Territory");
+    NT("Northern Territory"),
+    CC("Cocos (Keeling) Islands"),
+    CX("Christmas Island"),
+    AC("Ashmore and Cartier Islands"),
+    CS("Coral Sea Islands"),
+    NF("Norfolk Island"),
+    HM("Heard and McDonald Islands"),
+    AQ("Australian Antartic Territory"),
+    TSPZ("Torres Strait Protected Zone");
     
     private String value;
     
