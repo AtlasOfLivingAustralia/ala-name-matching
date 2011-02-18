5c8e7a3a5a1f1c2cdecf7b96099c5418099a911f
peter.flemming@csiro.au <peter.flemming@csiro.au>
2011-02-18 06:51:17 +0000

    Additional fields
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/sensitive-species@1861 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/java/au/org/ala/sds/model/SensitivityInstance.java
+++ src/main/java/au/org/ala/sds/model/SensitivityInstance.java
@@ -1,32 +1,59 @@
 package au.org.ala.sds.model;
 
+import java.util.Date;
+
+import au.org.ala.sds.util.DateHelper;
+
 /**
 *
 * @author Peter Flemming (peter.flemming@csiro.au)
 */
 public class SensitivityInstance {
 
-    private ConservationCategory category;
-    private String dataProvider;
+    private SensitivityCategory category;
+    private String authority;
     private SensitivityZone zone;
+    private Date fromDate;
+    private Date toDate;
     
-    public SensitivityInstance(ConservationCategory category, String dataProvider, SensitivityZone zone) {
+    public SensitivityInstance(SensitivityCategory category, String authority, String fromDate, String toDate, SensitivityZone zone) {
         this.category = category;
-        this.dataProvider = dataProvider;
+        this.authority = authority;
+        this.fromDate = fromDate == null ? null : DateHelper.parseDate(fromDate);
+        this.toDate = toDate == null ? null : DateHelper.parseDate(toDate);
         this.zone = zone;
     }
 
-    public ConservationCategory getCategory() {
+    public SensitivityCategory getCategory() {
         return category;
     }
 
-    public String getDataProvider() {
-        return dataProvider;
+    public String getAuthority() {
+        return authority;
     }
 
     public SensitivityZone getZone() {
         return zone;
     }
 
-    
+    public void setZone(SensitivityZone zone) {
+        this.zone = zone;
+    }
+
+     public Date getToDate() {
+        return toDate;
+    }
+
+    public void setToDate(Date toDate) {
+        this.toDate = toDate;
+    }
+
+    public Date getFromDate() {
+        return fromDate;
+    }
+
+    public void setFromDate(Date fromDate) {
+        this.fromDate = fromDate;
+    }
+
 }
