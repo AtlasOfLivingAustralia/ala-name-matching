5623f1bca9f676be56eab42d35921a64906a1ee6
peter.flemming@csiro.au <peter.flemming@csiro.au>
2011-02-23 00:03:06 +0000

    Updated test to not require dependency injection
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/sensitive-species@1898 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/test/java/au/org/ala/sds/GeneraliseTest.java
+++ src/test/java/au/org/ala/sds/GeneraliseTest.java
@@ -17,14 +17,16 @@ package au.org.ala.sds;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import javax.sql.DataSource;
 
-import au.org.ala.sds.SensitiveSpeciesFinder;
+import org.apache.commons.dbcp.BasicDataSource;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import au.org.ala.checklist.lucene.CBIndexSearch;
 import au.org.ala.sds.model.SensitiveSpecies;
 import au.org.ala.sds.util.GeneralisedLocation;
+import au.org.ala.sds.util.GeneralisedLocationFactory;
 
 /**
  *
@@ -32,18 +34,25 @@ import au.org.ala.sds.util.GeneralisedLocation;
  */
 public class GeneraliseTest {
 
-    ApplicationContext context;
-    SensitiveSpeciesFinder finder;
-    
-    @Before
-    public void setUp() throws Exception {
-        context = new ClassPathXmlApplicationContext("spring-config.xml");
-        finder = context.getBean("searchImpl", SensitiveSpeciesFinder.class);
+    static DataSource dataSource;
+    static CBIndexSearch cbIndexSearch;
+    static SensitiveSpeciesFinder finder;
+
+    @BeforeClass
+    public static void runOnce() throws Exception {
+        dataSource = new BasicDataSource();
+        ((BasicDataSource) dataSource).setDriverClassName("com.mysql.jdbc.Driver");
+        ((BasicDataSource) dataSource).setUrl("jdbc:mysql://localhost/portal");
+        ((BasicDataSource) dataSource).setUsername("root");
+        ((BasicDataSource) dataSource).setPassword("password");
+
+        cbIndexSearch = new CBIndexSearch("/data/lucene/namematching");
+        finder = SensitiveSpeciesFinderFactory.getSensitiveSpeciesFinder(dataSource, cbIndexSearch);
     }
 
     @Test
     public void testGeneralisation() {
-        
+
         //
         // Endangered Birds Australia species in ACT - position generalised
         //
@@ -51,41 +60,41 @@ public class GeneraliseTest {
         assertNotNull(ss);
         String latitude = "-35.276771";   // Epicorp
         String longitude = "149.112539";
-        GeneralisedLocation gl = new GeneralisedLocation(latitude, longitude, ss.getConservationCategory(latitude, longitude));
+        GeneralisedLocation gl = GeneralisedLocationFactory.getGeneralisedLocation(latitude, longitude, ss.getConservationCategory(latitude, longitude));
         assertEquals("Latitude", "-35.3", gl.getGeneralisedLatitude());
         assertEquals("Longitude", "149.1", gl.getGeneralisedLongitude());
         assertEquals("InMetres", "10000", gl.getGeneralisationInMetres());
-        
+
         //
         // Critically endangered NSW species in ACT - not generalised
         //
         ss = finder.findSensitiveSpecies("Wollemia nobilis");
         assertNotNull(ss);
-        gl = new GeneralisedLocation(latitude, longitude, ss.getConservationCategory(latitude, longitude));
+        gl = GeneralisedLocationFactory.getGeneralisedLocation(latitude, longitude, ss.getConservationCategory(latitude, longitude));
         assertEquals("Latitude", "-35.276771", gl.getGeneralisedLatitude());
         assertEquals("Longitude", "149.112539", gl.getGeneralisedLongitude());
         assertEquals("InMetres", "", gl.getGeneralisationInMetres());
-        
+
         //
         // Critically endangered NSW species in NZ - not generalised
         //
         latitude = "-41.538137";    // NZ
         longitude = "173.968817";
-        gl = new GeneralisedLocation(latitude, longitude, ss.getConservationCategory(latitude, longitude));
+        gl = GeneralisedLocationFactory.getGeneralisedLocation(latitude, longitude, ss.getConservationCategory(latitude, longitude));
         assertEquals("Latitude", "-41.538137", gl.getGeneralisedLatitude());
         assertEquals("Longitude", "173.968817", gl.getGeneralisedLongitude());
         assertEquals("InMetres", "", gl.getGeneralisationInMetres());
-         
+
         //
         // Critically endangered NSW species in NSW - position not published
         //
         latitude = "-33.630629";    // NSW
         longitude = "150.441284";
-        gl = new GeneralisedLocation(latitude, longitude, ss.getConservationCategory(latitude, longitude));
+        gl = GeneralisedLocationFactory.getGeneralisedLocation(latitude, longitude, ss.getConservationCategory(latitude, longitude));
         assertEquals("Latitude", "", gl.getGeneralisedLatitude());
         assertEquals("Longitude", "", gl.getGeneralisedLongitude());
         assertEquals("InMetres", "", gl.getGeneralisationInMetres());
-         
+
         //
         // Endangered TAS species in TAS - generalised
         //
@@ -93,10 +102,20 @@ public class GeneraliseTest {
         assertNotNull(ss);
         latitude = "-40.111689";    // TAS
         longitude = "148.095703";
-        gl = new GeneralisedLocation(latitude, longitude, ss.getConservationCategory(latitude, longitude));
+        gl = GeneralisedLocationFactory.getGeneralisedLocation(latitude, longitude, ss.getConservationCategory(latitude, longitude));
         assertEquals("Latitude", "-40.1", gl.getGeneralisedLatitude());
         assertEquals("Longitude", "148.1", gl.getGeneralisedLongitude());
         assertEquals("InMetres", "10000", gl.getGeneralisationInMetres());
-        
+
+        //
+        // Find sensitive species by LSID
+        //
+        latitude = "-33.630629";    // NSW
+        longitude = "150.441284";
+        ss = finder.findSensitiveSpeciesByLsid("urn:lsid:biodiversity.org.au:afd.taxon:fb2de285-c58c-4c63-9268-9beef7c61c16");
+        gl = GeneralisedLocationFactory.getGeneralisedLocation(latitude, longitude, ss, "NSW");
+        assertEquals("Latitude", "-33.6", gl.getGeneralisedLatitude());
+        assertEquals("Longitude", "150.4", gl.getGeneralisedLongitude());
+        assertEquals("InMetres", "10000", gl.getGeneralisationInMetres());
     }
 }
