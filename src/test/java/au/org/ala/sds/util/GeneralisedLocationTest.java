9edebbe74a62c38f1733eea0bcb9ee83a58d5d86
peter.flemming@csiro.au <peter.flemming@csiro.au>
2011-02-25 07:03:39 +0000

    Changed GeneralisedLocation API.
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/sensitive-species@1936 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/test/java/au/org/ala/sds/util/GeneralisedLocationTest.java
+++ src/test/java/au/org/ala/sds/util/GeneralisedLocationTest.java
@@ -2,41 +2,59 @@ package au.org.ala.sds.util;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
+
 import org.junit.Test;
 
+import au.org.ala.sds.model.SensitiveSpecies;
 import au.org.ala.sds.model.SensitivityCategory;
+import au.org.ala.sds.model.SensitivityInstance;
+import au.org.ala.sds.model.SensitivityZone;
 
 
 public class GeneralisedLocationTest {
 
-    static String LAT = "-35.12345";
-    static String LONG = "146.67890";
+    String NSW_LAT = "-35.12345",  NSW_LONG = "146.67890";
+    String VIC_LAT = "-37.046409", VIC_LONG = "146.239014";
+    String QLD_LAT = "-18.406655", QLD_LONG = "146.2541";
+    String TAS_LAT = "-42.067518", TAS_LONG = "145.278568";
+    String ACT_LAT = "-35.29187",  ACT_LONG = "149.100137";
+    static SensitiveSpecies ss;
+
+    static {
+        ss = new SensitiveSpecies("Crex crex");
+        List<SensitivityInstance> instances = ss.getInstances();
+        instances.add(new SensitivityInstance(SensitivityCategory.CRITICALLY_ENDANGERED, "NSW DECCW", null, null, SensitivityZone.NSW, "WITHHOLD"));
+        instances.add(new SensitivityInstance(SensitivityCategory.ENDANGERED, "Vic DSE", null, null, SensitivityZone.VIC, "10km"));
+        instances.add(new SensitivityInstance(SensitivityCategory.VULNERABLE, "QLD DERM", null, null, SensitivityZone.QLD, "1km"));
+        instances.add(new SensitivityInstance(SensitivityCategory.NEAR_THREATENED, "Tas DPIPWE", null, null, SensitivityZone.TAS, "100m"));
+    }
 
     @Test
     public void TestLocationGeneralisation() {
-        GeneralisedLocation extremeGeneralisation = GeneralisedLocationFactory.getGeneralisedLocation(LAT, LONG, SensitivityCategory.CRITICALLY_ENDANGERED);
+        GeneralisedLocation extremeGeneralisation = GeneralisedLocationFactory.getGeneralisedLocation(NSW_LAT, NSW_LONG, ss);
         assertEquals("", extremeGeneralisation.getGeneralisedLatitude());
         assertEquals("", extremeGeneralisation.getGeneralisedLongitude());
         assertEquals("", extremeGeneralisation.getGeneralisationInMetres());
 
-        GeneralisedLocation highGeneralisation = GeneralisedLocationFactory.getGeneralisedLocation(LAT, LONG, SensitivityCategory.ENDANGERED);
-        assertEquals("-35.1", highGeneralisation.getGeneralisedLatitude());
-        assertEquals("146.7", highGeneralisation.getGeneralisedLongitude());
+        GeneralisedLocation highGeneralisation = GeneralisedLocationFactory.getGeneralisedLocation(VIC_LAT, VIC_LONG, ss);
+        assertEquals("-37.0", highGeneralisation.getGeneralisedLatitude());
+        assertEquals("146.2", highGeneralisation.getGeneralisedLongitude());
         assertEquals("10000", highGeneralisation.getGeneralisationInMetres());
 
-        GeneralisedLocation mediumGeneralisation = GeneralisedLocationFactory.getGeneralisedLocation(LAT, LONG, SensitivityCategory.VULNERABLE);
-        assertEquals("-35.12", mediumGeneralisation.getGeneralisedLatitude());
-        assertEquals("146.68", mediumGeneralisation.getGeneralisedLongitude());
+        GeneralisedLocation mediumGeneralisation = GeneralisedLocationFactory.getGeneralisedLocation(QLD_LAT, QLD_LONG, ss);
+        assertEquals("-18.41", mediumGeneralisation.getGeneralisedLatitude());
+        assertEquals("146.25", mediumGeneralisation.getGeneralisedLongitude());
         assertEquals("1000", mediumGeneralisation.getGeneralisationInMetres());
 
-        GeneralisedLocation lowGeneralisation = GeneralisedLocationFactory.getGeneralisedLocation(LAT, LONG, SensitivityCategory.NEAR_THREATENED);
-        assertEquals("-35.123", lowGeneralisation.getGeneralisedLatitude());
-        assertEquals("146.679", lowGeneralisation.getGeneralisedLongitude());
+        GeneralisedLocation lowGeneralisation = GeneralisedLocationFactory.getGeneralisedLocation(TAS_LAT, TAS_LONG, ss);
+        assertEquals("-42.068", lowGeneralisation.getGeneralisedLatitude());
+        assertEquals("145.279", lowGeneralisation.getGeneralisedLongitude());
         assertEquals("100", lowGeneralisation.getGeneralisationInMetres());
 
-        GeneralisedLocation defaultGeneralisation = GeneralisedLocationFactory.getGeneralisedLocation(LAT, LONG, SensitivityCategory.LEAST_CONCERN);
-        assertEquals(LAT, defaultGeneralisation.getGeneralisedLatitude());
-        assertEquals(LONG, defaultGeneralisation.getGeneralisedLongitude());
+        GeneralisedLocation defaultGeneralisation = GeneralisedLocationFactory.getGeneralisedLocation(ACT_LAT, ACT_LONG, ss);
+        assertEquals(ACT_LAT, defaultGeneralisation.getGeneralisedLatitude());
+        assertEquals(ACT_LONG, defaultGeneralisation.getGeneralisedLongitude());
         assertEquals("", defaultGeneralisation.getGeneralisationInMetres());
     }
 }
