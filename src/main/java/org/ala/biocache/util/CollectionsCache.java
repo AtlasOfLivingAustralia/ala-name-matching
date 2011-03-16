1e932f7247fc49f71bb659d05712ebc97b95ca97
nickdos <nickdos@gmail.com>
2011-03-16 04:06:42 +0000

    Added CollectionCache class (removed from hubs-webapp).
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/biocache-service@2015 1ef1b718-b4f5-11dd-90a5-ed332e521da9

---
+++ src/main/java/org/ala/biocache/util/CollectionsCache.java
@@ -0,0 +1,165 @@
+/* *************************************************************************
+ *  Copyright (C) 2011 Atlas of Living Australia
+ *  All Rights Reserved.
+ * 
+ *  The contents of this file are subject to the Mozilla Public
+ *  License Version 1.1 (the "License"); you may not use this file
+ *  except in compliance with the License. You may obtain a copy of
+ *  the License at http://www.mozilla.org/MPL/
+ * 
+ *  Software distributed under the License is distributed on an "AS
+ *  IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ *  implied. See the License for the specific language governing
+ *  rights and limitations under the License.
+ ***************************************************************************/
+
+package org.ala.biocache.util;
+
+import java.lang.reflect.Field;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
+import javax.inject.Inject;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestOperations;
+
+/**
+ * Provides access to the collection and institution codes and names from the Collectory.
+ * Uses the Collectory webservices to get a map of codes & names for institutions and collections
+ * and caches these. Cache is automatically updated after a configurable timeout period.
+ *
+ * @author "Nick dos Remedios <Nick.dosRemedios@csiro.au>"
+ */
+@Component("collectionsCache")
+public class CollectionsCache {
+    protected LinkedHashMap<String, String> institutions = new LinkedHashMap<String, String>();
+    protected LinkedHashMap<String, String> collections = new LinkedHashMap<String, String>();
+    protected Date lastUpdated = new Date();
+    protected Long timeout = 3600000L; // in millseconds (1 hour)
+    protected String collectoryUriPrefix = "http://collections.ala.org.au/ws/";
+
+    /** Spring injected RestTemplate object */
+    @Inject
+    private RestOperations restTemplate; // NB MappingJacksonHttpMessageConverter() injected by Spring
+    /** Log4J logger */
+    private final static Logger logger = Logger.getLogger(CollectionsCache.class);
+
+    /**
+     * Get the institutions
+     *
+     * @return
+     */
+    public LinkedHashMap<String, String> getInstitutions() {
+        checkCacheAge();
+        return this.institutions;
+    }
+
+    /**
+     * Get the collections
+     *
+     * @return
+     */
+    public LinkedHashMap<String, String> getCollections() {
+        checkCacheAge();
+        return this.collections;
+    }
+
+    /**
+     * Check age of cache and retrieve new values from Collections webservices if needed.
+     */
+    protected void checkCacheAge() {
+        Date currentDate = new Date();
+        Long timeSinceUpdate = currentDate.getTime() - lastUpdated.getTime();
+        logger.debug("timeSinceUpdate = " + timeSinceUpdate);
+        
+        if (timeSinceUpdate > this.timeout || institutions.size() < 1 || collections.size() < 1) {
+            updateCache();
+            lastUpdated = new Date(); // update timestamp
+        }
+    }
+
+    /**
+     * Update the entity types (fields)
+     */
+    protected void updateCache() {
+        logger.info("Updating collectory cache...");
+        this.collections = getCodesMap(ResourceType.COLLECTION);
+        this.institutions = getCodesMap(ResourceType.INSTITUTION);
+
+    }
+
+    /**
+     * Do the web services call. Uses RestTemplate.
+     *
+     * @param type
+     * @return
+     */
+    protected LinkedHashMap getCodesMap(ResourceType type) {
+        LinkedHashMap<String, String> entityMap = null;
+
+        try {
+            // grab cached values (map) in case WS is not available (uses reflection)
+            Field f = CollectionsCache.class.getDeclaredField(type.getType() + "s"); // field is plural form
+            entityMap = (LinkedHashMap<String, String>) f.get(this);
+            logger.debug("checking map size: " + entityMap.size());
+        } catch (Exception ex) {
+            logger.error("Java reflection error: " + ex.getMessage(), ex);
+        }
+
+        try {
+            entityMap = new LinkedHashMap<String, String>(); // reset now we're inside the try
+            final String jsonUri = collectoryUriPrefix + "dataHub/dh1/" + type.getType() + "s.json";
+            logger.debug("Requesting: " + jsonUri);
+            List<LinkedHashMap<String, String>> entities = restTemplate.getForObject(jsonUri, List.class);
+            logger.debug("number of entities = " + entities.size());
+
+            for (LinkedHashMap<String, String> je : entities) {
+                entityMap.put(je.get("uid"), je.get("name"));
+                logger.debug("uid = " + je.get("uid") + " & name = " + je.get("name"));
+            }
+        } catch (Exception ex) {
+            logger.error("RestTemplate error: " + ex.getMessage(), ex);
+        }
+
+        return entityMap;
+    }
+    
+    /**
+     * Inner enum class
+     */
+    public enum ResourceType {
+        INSTITUTION("institution"),
+        COLLECTION("collection");
+
+        private String type;
+
+        ResourceType(String type) {
+            this.type = type;
+        }
+
+        public String getType() {
+            return type;
+        }
+    }
+
+    /*
+     * Getter and setters
+     */
+    public String getCollectoryUriPrefix() {
+        return collectoryUriPrefix;
+    }
+
+    public void setCollectoryUriPrefix(String collectoryUriPrefix) {
+        this.collectoryUriPrefix = collectoryUriPrefix;
+    }
+
+    public Long getTimeout() {
+        return timeout;
+    }
+
+    public void setTimeout(Long timeout) {
+        this.timeout = timeout;
+    }
+
+}
