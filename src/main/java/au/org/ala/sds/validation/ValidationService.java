0def6fc35cc7f0c68dda8a14d2a8e03dfbe09beb
peter.flemming@csiro.au <peter.flemming@csiro.au>
2011-03-21 01:40:25 +0000

    Interface changed to use FactCollection for input parameters.
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/sensitive-species@2051 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/java/au/org/ala/sds/validation/ValidationService.java
+++ src/main/java/au/org/ala/sds/validation/ValidationService.java
@@ -1,12 +1,9 @@
 package au.org.ala.sds.validation;
 
-import java.util.Date;
-
 import au.org.ala.sds.model.SensitiveSpecies;
-import au.org.ala.sds.model.SensitivityZone;
 
 public interface ValidationService {
 
-    ValidationOutcome validate(SensitiveSpecies ss, SensitivityZone act, Date date);
+    ValidationOutcome validate(SensitiveSpecies sensitiveSpecies, FactCollection facts);
 
 }
