d83faa1ed18a327dd70e7f12f1c5367033d9c284
peter.flemming@csiro.au <peter.flemming@csiro.au>
2011-03-21 00:14:18 +0000

    Removed Spring framework dependency.
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/sensitive-species@2028 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/java/au/org/ala/sds/dao/RawOccurrenceDaoImpl.java
+++ src/main/java/au/org/ala/sds/dao/RawOccurrenceDaoImpl.java
@@ -18,13 +18,11 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-import javax.inject.Inject;
 import javax.sql.DataSource;
 
 import org.apache.log4j.Logger;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
-import org.springframework.stereotype.Component;
 
 import au.org.ala.sds.dto.SpeciesOccurrenceDto;
 
@@ -32,13 +30,12 @@ import au.org.ala.sds.dto.SpeciesOccurrenceDto;
  *
  * @author Peter Flemming (peter.flemming@csiro.au)
  */
-@Component
 public class RawOccurrenceDaoImpl extends JdbcDaoSupport implements RawOccurrenceDao {
-    
+
     protected static final Logger logger = Logger.getLogger(RawOccurrenceDaoImpl.class);
-    
+
     public static final String SELECT_ALL_OCCURRENCES = "SELECT id, scientific_name, latitude, longitude FROM raw_occurrence_record";
-    
+
     public static final String UPDATE_OCCURRENCE = "UPDATE raw_occurrence_record " + "" +
                                                    "SET latitude=?, longitude=?, generalised_metres=?, raw_latitude=?, raw_longitude=? " +
                                                    "WHERE id=?";
@@ -47,12 +44,11 @@ public class RawOccurrenceDaoImpl extends JdbcDaoSupport implements RawOccurrenc
                                                         "SET latitude=?, longitude=?, generalised_metres=? " +
                                                         "WHERE id=?";
 
-    @Inject
     public RawOccurrenceDaoImpl(DataSource dataSource) {
         this.setDataSource(dataSource);
         logger.debug("Occurrence data source set - " + dataSource.toString());
     }
-    
+
     @Override
     public void updateLocation(int id, String generalisedLat, String generalisedLong, String generalisedMetres, String rawLat, String rawLong ) {
         int rows = getJdbcTemplate().update(UPDATE_OCCURRENCE, new Object [] { generalisedLat, generalisedLong, generalisedMetres, rawLat, rawLong, new Integer(id) });
@@ -71,7 +67,7 @@ public class RawOccurrenceDaoImpl extends JdbcDaoSupport implements RawOccurrenc
 
     @Override
     public List<SpeciesOccurrenceDto> getOccurrences() {
-        return (List<SpeciesOccurrenceDto>) getJdbcTemplate().query(
+        return getJdbcTemplate().query(
                 SELECT_ALL_OCCURRENCES,
                 new Object [] {},
                 new RowMapper<SpeciesOccurrenceDto>() {
