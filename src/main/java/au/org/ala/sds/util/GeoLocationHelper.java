fb705b0cf876fe8f613b8b143bc2d0008aedf34c
peter.flemming@csiro.au <peter.flemming@csiro.au>
2011-03-21 00:53:11 +0000

    Refactored to return a set of zones from lat/long lookup.  Also added work around for Papaya Fruit Fly PQA and Emerald Citrus Canker PQA.
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/sensitive-species@2041 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/java/au/org/ala/sds/util/GeoLocationHelper.java
+++ src/main/java/au/org/ala/sds/util/GeoLocationHelper.java
@@ -3,9 +3,12 @@ package au.org.ala.sds.util;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.zip.GZIPInputStream;
 
+import org.apache.commons.lang.math.NumberUtils;
 import org.apache.log4j.Logger;
 import org.jdom.Document;
 import org.jdom.Element;
@@ -16,22 +19,22 @@ import au.org.ala.sds.model.SensitivityZone;
 public class GeoLocationHelper {
 
     @SuppressWarnings("unchecked")
-    public static SensitivityZone getStateContainingPoint(String latitude, String longitude) throws Exception {
+    public static Set<SensitivityZone> getZonesContainingPoint(String latitude, String longitude) throws Exception {
 
         final Logger logger = Logger.getLogger(GeoLocationHelper.class);
-        SensitivityZone state = null;
-        
+        Set<SensitivityZone> zones = new HashSet<SensitivityZone>();;
+
         //
         // Call geospatial web service
         //
-        URL url = new URL("http://spatial.ala.org.au/gazetteer/state/latlon/" + latitude + "," + longitude);
+        URL url = new URL("http://spatial.ala.org.au/gazetteer/latlon/" + latitude + "," + longitude);
         URLConnection connection = url.openConnection();
         connection.setRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
         connection.setRequestProperty("Accept-Encoding", "gzip,deflate");
         InputStream inStream = connection.getInputStream();
         GZIPInputStream gis = new GZIPInputStream(inStream);
 
-//        String response = 
+//        String response =
 //                  "<search xmlns:xlink='http://www.w3.org/1999/xlink'>"
 //                + " <results>"
 //                + "     <result xlink:href='/geoserver/rest/gazetteer/aus1/New_South_Wales.json'>"
@@ -45,7 +48,7 @@ public class GeoLocationHelper {
 //                + " </results>"
 //                + "</search>";
 //        InputStream in = new ByteArrayInputStream(response.getBytes());
-        
+
         //
         // Parse XML result
         //
@@ -55,14 +58,38 @@ public class GeoLocationHelper {
         List<Element> results = root.getChild("results").getChildren();
         for (Element result : results) {
             String name = result.getChildText("name");
-            state = SensitivityZone.getZone(name);
+            String layer = result.getChildText("layerName");
+            if (layer.equalsIgnoreCase("state")) {
+                SensitivityZone zone;
+                if ((zone = SensitivityZone.getZone(name)) != null) {
+                    zones.add(zone);
+                }
+
+                // PFF PQA
+                if (name.equalsIgnoreCase("Queensland") &&
+                    layer.equalsIgnoreCase("state") &&
+                    NumberUtils.toFloat(latitude) >= -19.0 &&
+                    NumberUtils.toFloat(longitude) >= 144.25) {
+                    zones.add(SensitivityZone.PFFPQA1995);
+                }
+            }
+
+            // Emerald Citrus Canker PQA
+            if (layer.equalsIgnoreCase("lga") &&
+                    (name.equalsIgnoreCase("Bauhinia (Queensland)") ||
+                     name.equalsIgnoreCase("Emerald (Queensland)") ||
+                     name.equalsIgnoreCase("Peak Downs (Queensland)"))
+               ) {
+                zones.add(SensitivityZone.ECCPQA2004);
+            }
         }
-        
-        if (state == null) {
-            logger.warn("State could not be determined from location: Lat " + latitude + ", Long " + longitude);
+
+        if (zones.isEmpty()) {
+            logger.warn("Zone could not be determined from location: Lat " + latitude + ", Long " + longitude);
+            zones.add(SensitivityZone.NOTAUS);
         }
-        
-        return state;
+
+        return zones;
     }
 
 }
