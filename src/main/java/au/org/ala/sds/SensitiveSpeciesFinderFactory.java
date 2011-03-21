39a6a63ef9cf7348c4cdd8f2d47fdc9c1fde9dff
peter.flemming@csiro.au <peter.flemming@csiro.au>
2011-03-21 00:12:44 +0000

    Added loading of sensitive species from XML file.
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/sensitive-species@2027 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/java/au/org/ala/sds/SensitiveSpeciesFinderFactory.java
+++ src/main/java/au/org/ala/sds/SensitiveSpeciesFinderFactory.java
@@ -7,7 +7,9 @@ import javax.sql.DataSource;
 
 import au.org.ala.checklist.lucene.CBIndexSearch;
 import au.org.ala.sds.dao.SensitiveSpeciesDao;
-import au.org.ala.sds.dao.SensitiveSpeciesDaoImpl;
+import au.org.ala.sds.dao.SensitiveSpeciesMySqlDao;
+import au.org.ala.sds.dao.SensitiveSpeciesXmlDao;
+import au.org.ala.sds.model.SensitiveSpeciesStore;
 
 /**
  *
@@ -15,9 +17,20 @@ import au.org.ala.sds.dao.SensitiveSpeciesDaoImpl;
  */
 public class SensitiveSpeciesFinderFactory {
 
-    public static SensitiveSpeciesFinder getSensitiveSpeciesFinder(DataSource dataSource, CBIndexSearch cbIndexSearch) throws Exception {
+    public static SensitiveSpeciesFinder getSensitiveSpeciesFinder(DataSource dataSource, CBIndexSearch cbIndexSearcher) throws Exception {
 
-        SensitiveSpeciesDao dao = new SensitiveSpeciesDaoImpl(dataSource, cbIndexSearch);
-        return new SensitiveSpeciesFinder(dao);
+        SensitiveSpeciesDao dao = new SensitiveSpeciesMySqlDao(dataSource);
+        SensitiveSpeciesStore store = new SensitiveSpeciesStore(dao, cbIndexSearcher);
+        return new SensitiveSpeciesFinder(store);
+
     }
+
+    public static SensitiveSpeciesFinder getSensitiveSpeciesFinder(String url, CBIndexSearch cbIndexSearcher) throws Exception {
+
+        SensitiveSpeciesDao dao = new SensitiveSpeciesXmlDao(url);
+        SensitiveSpeciesStore store = new SensitiveSpeciesStore(dao, cbIndexSearcher);
+        return new SensitiveSpeciesFinder(store);
+
+    }
+
 }
