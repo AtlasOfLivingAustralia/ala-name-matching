a853f17aff79cc557372bc654889c572f2e42647
djtfmartin <djtfmartin@gmail.com>
2011-04-28 07:26:57 +0000

    google guice integration
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/biocache-store@2125 1ef1b718-b4f5-11dd-90a5-ed332e521da9

---
+++ src/main/scala/au/org/ala/biocache/config.scala
@@ -0,0 +1,40 @@
+package au.org.ala.biocache
+
+import java.util.Properties
+import com.google.inject.name.Names
+import au.org.ala.checklist.lucene.CBIndexSearch
+import com.google.inject.{Scopes, Guice, Injector, AbstractModule}
+
+/**
+ * Simple singleton wrapper for Guice (or spring)
+ */
+object Config {
+    private val inj:Injector = Guice.createInjector(new ConfigModule())
+    def getInstance(classs:Class[_]) = inj.getInstance(classs)
+}
+
+/**
+ * Guice module
+ */
+class ConfigModule extends AbstractModule {
+
+    override def configure() {
+
+        val properties = new Properties()
+        properties.load(this.getClass.getResourceAsStream("/biocache.properties"))
+        Names.bindProperties(this.binder,properties)
+
+        //bind concrete implementations
+        bind(classOf[OccurrenceDAO]).to(classOf[OccurrenceDAOImpl]).in(Scopes.SINGLETON)
+        bind(classOf[IndexDAO]).to(classOf[SolrOccurrenceDAOImpl]).in(Scopes.SINGLETON)
+
+        val nameIndex = new CBIndexSearch(properties.getProperty("nameIndexLocation"))
+        bind(classOf[CBIndexSearch]).toInstance(nameIndex)
+
+        properties.getProperty("db") match {
+            case "cassandra" => bind(classOf[PersistenceManager]).to(classOf[CassandraPersistenceManager]).in(Scopes.SINGLETON)
+            case "mongodb" => bind(classOf[PersistenceManager]).to(classOf[MongoDBPersistenceManager]).in(Scopes.SINGLETON)
+            case _ => bind(classOf[PersistenceManager]).to(classOf[CassandraPersistenceManager]).in(Scopes.SINGLETON)
+        }
+    }
+}
