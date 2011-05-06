4108690e268c18660c9ecb97ecd343f9565a3141
peter.flemming@csiro.au <peter.flemming@csiro.au>
2011-05-06 07:20:17 +0000

    Additional messages and convenience methods
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/sensitive-species@2166 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/java/au/org/ala/sds/validation/MessageFactory.java
+++ src/main/java/au/org/ala/sds/validation/MessageFactory.java
@@ -20,25 +20,43 @@ import au.org.ala.sds.model.SdsMessage;
 public class MessageFactory {
 
 	// Message key constants
-    public static final String PLANT_PEST_MSG_CAT1_01 = "PPC1-01";
-    public static final String PLANT_PEST_MSG_CAT1_02 = "PPC1-02";
-    public static final String PLANT_PEST_MSG_CAT1_03 = "PPC1-03";
-    public static final String PLANT_PEST_MSG_CAT1_04 = "PPC1-04";
+    public static final String LOCATION_INVALID = "LatLongInvalid";
+    public static final String LOCATION_MISSING = "LatLongMissing";
+    public static final String STATE_INVALID = "StateInvalid";
+    public static final String NOT_AUSTRALIA = "NotAus";
 
-    public static final String PLANT_PEST_MSG_CAT2_01 = "PPC2-01";
-    public static final String PLANT_PEST_MSG_CAT2_02 = "PPC2-02";
-    public static final String PLANT_PEST_MSG_CAT2_03 = "PPC2-03";
-    public static final String PLANT_PEST_MSG_CAT2_04 = "PPC2-04";
+    public static final String PLANT_PEST_MSG_CAT1_A1 = "PPC1-A1";
+    public static final String PLANT_PEST_MSG_CAT1_A2 = "PPC1-A2";
+    public static final String PLANT_PEST_MSG_CAT1_A3 = "PPC1-A3";
+    public static final String PLANT_PEST_MSG_CAT1_A4 = "PPC1-A4";
+    public static final String PLANT_PEST_MSG_CAT1_B1 = "PPC1-B1";
+    public static final String PLANT_PEST_MSG_CAT1_D1 = "PPC1-D1";
 
-    public static final String PLANT_PEST_MSG_CAT3_01 = "PPC3-01";
-    public static final String PLANT_PEST_MSG_CAT3_02 = "PPC3-02";
-    public static final String PLANT_PEST_MSG_CAT3_03 = "PPC3-03";
-    public static final String PLANT_PEST_MSG_CAT3_04 = "PPC3-04";
+    public static final String PLANT_PEST_MSG_CAT2_A1 = "PPC2-A1";
+    public static final String PLANT_PEST_MSG_CAT2_B1 = "PPC2-B1";
+    public static final String PLANT_PEST_MSG_CAT2_B2 = "PPC2-B2";
 
-    public static final String PLANT_PEST_MSG_CAT4_01 = "PPC4-01";
-    public static final String PLANT_PEST_MSG_CAT4_02 = "PPC4-02";
-    public static final String PLANT_PEST_MSG_CAT4_03 = "PPC4-03";
-    public static final String PLANT_PEST_MSG_CAT4_04 = "PPC4-04";
+    public static final String PLANT_PEST_MSG_CAT3_A1 = "PPC3-A1";
+    public static final String PLANT_PEST_MSG_CAT3_B1 = "PPC3-B1";
+    public static final String PLANT_PEST_MSG_CAT3_B2 = "PPC3-B2";
+
+    public static final String PLANT_PEST_MSG_CAT4_A1 = "PPC4-A1";
+    public static final String PLANT_PEST_MSG_CAT4_B1 = "PPC4-B1";
+    public static final String PLANT_PEST_MSG_CAT4_B2 = "PPC4-B2";
+    public static final String PLANT_PEST_MSG_CAT4_B3 = "PPC4-B3";
+
+    public static final String PLANT_PEST_MSG_CAT5A_A1 = "PPC5a-A1";
+    public static final String PLANT_PEST_MSG_CAT5A_A2 = "PPC5a-A2";
+    public static final String PLANT_PEST_MSG_CAT5A_B1 = "PPC5a-B1";
+    public static final String PLANT_PEST_MSG_CAT5A_B2 = "PPC5a-B2";
+    public static final String PLANT_PEST_MSG_CAT5A_C1 = "PPC5a-C1";
+    public static final String PLANT_PEST_MSG_CAT5A_C2 = "PPC5a-C2";
+
+    public static final String PLANT_PEST_MSG_CAT5B_A1 = "PPC5b-A1";
+    public static final String PLANT_PEST_MSG_CAT5B_A2 = "PPC5b-A2";
+
+    public static final String PLANT_PEST_MSG_CAT6_A1 = "PPC6-A1";
+    public static final String PLANT_PEST_MSG_CAT6_A2 = "PPC6-A2";
 
     // Resource file is sds-messages.properties
     private static String BUNDLE_NAME = "sds-messages";
@@ -59,7 +77,65 @@ public class MessageFactory {
      * @return The created message.
      */
     public static Message createMessage(Message.Type type, String messageKey, Object... context) {
-        return new SdsMessage(type, getMessageText(messageKey, context));
+        return new SdsMessage(type, getMessageText(messageKey, context) + ". [" + messageKey + "]");
+    }
+
+    /**
+     * Convenience methods.
+     *
+     * @param messageKey The message key for the text definition.
+     * @param context Varargs of context parameters.
+     * @return The created message.
+     */
+    public static Message createInfoMessage(String messageKey, Object... context) {
+        return createMessage(Message.Type.INFO, messageKey, context);
+    }
+
+    public static Message createErrorMessage(String messageKey, Object... context) {
+        return createMessage(Message.Type.ERROR, messageKey, context);
+    }
+
+    public static Message createAlertMessage(String messageKey, Object... context) {
+        return createMessage(Message.Type.ALERT, messageKey, context);
+    }
+
+    public static Message createWarningMessage(String messageKey, Object... context) {
+        return createMessage(Message.Type.WARNING, messageKey, context);
+    }
+
+    /**
+     * Create a message using the message key to get the message text.
+     * No context parameters are provided.
+     *
+     * @param type The message type.
+     * @param messageKey The message key for the text definition.
+     * @return The created message.
+     */
+    public static Message createMessage(Message.Type type, String messageKey) {
+        return new SdsMessage(type, getMessageText(messageKey, new Object [] {}) + ". [" + messageKey + "]");
+    }
+
+    /**
+     * Convenience methods.
+     *
+     * @param type The message type.
+     * @param messageKey The message key for the text definition.
+     * @return The created message.
+     */
+    public static Message createInfoMessage(String messageKey) {
+        return createMessage(Message.Type.INFO, messageKey);
+    }
+
+    public static Message createErrorMessage(String messageKey) {
+        return createMessage(Message.Type.ERROR, messageKey);
+    }
+
+    public static Message createAlertMessage(String messageKey) {
+        return createMessage(Message.Type.ALERT, messageKey);
+    }
+
+    public static Message createWarningMessage(String messageKey) {
+        return createMessage(Message.Type.WARNING, messageKey);
     }
 
     /**
@@ -72,25 +148,38 @@ public class MessageFactory {
      * <p>See <code>java.text.MessageFormat</code> for formatting options.
      *
      * @param messageKey The key that identifies the message.
-     * @param context An array of <code>Object</code> containing substitution parameters. May be null.
+     * @param context Varargs of context parameters.
      * @return The constructed message text.
      */
-    private static String getMessageText(String messageKey, Object... context) {
+    public static String getMessageText(String messageKey, Object... context) {
         String msgTemplate;
 
         try {
         	msgTemplate = MessagesHolder.messages.getString(messageKey);
         } catch(MissingResourceException mre) {
-            msgTemplate = "";
+            msgTemplate = "Message definition not found for supplied message key (" + messageKey + ")";
         }
 
-        if (msgTemplate.equals("")) {
-        	msgTemplate = "Message definition not found for supplied message key.";
-        }
-
-        return messageKey + " - " + MessageFormat.format(msgTemplate, context);
+        return MessageFormat.format(msgTemplate, context);
     }
 
+    /**
+     * Get message text by keyed lookup on <code>ResourceBundle</code>.
+     *
+     * @param messageKey The key that identifies the message.
+     * @return The message text.
+     */
+    public static String getMessageText(String messageKey) {
+        String msgTemplate;
+
+        try {
+            msgTemplate = MessagesHolder.messages.getString(messageKey);
+        } catch(MissingResourceException mre) {
+            msgTemplate = "Message definition not found for supplied message key (" + messageKey + ")";
+        }
+
+        return msgTemplate;
+    }
     /**
      * Override default message bundle name.  For testing purposes only.
      *
