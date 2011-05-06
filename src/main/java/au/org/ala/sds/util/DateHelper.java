2936d2a51d97e33dd43be2651220ea7d1641f047
peter.flemming@csiro.au <peter.flemming@csiro.au>
2011-05-06 07:10:58 +0000

    Additional date helper methods
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/sensitive-species@2156 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/java/au/org/ala/sds/util/DateHelper.java
+++ src/main/java/au/org/ala/sds/util/DateHelper.java
@@ -1,6 +1,8 @@
 package au.org.ala.sds.util;
 
+import java.text.DateFormat;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.apache.commons.lang.time.DateUtils;
@@ -10,10 +12,34 @@ public class DateHelper {
     public static Date parseDate(String date) {
         Date parsedDate = null;
         try {
-            parsedDate = DateUtils.parseDateStrictly(date, new String[] {"yyyy-MM-dd"});
+            parsedDate = DateUtils.parseDateStrictly(date, new String[] {"yyyy-MM-dd", "dd/mm/yy"});
         } catch (ParseException pe) {
             throw new IllegalArgumentException("Date " + date + " cannot be parsed", pe);
         }
         return parsedDate;
+    }
+
+    public static String formattedIso8601Date(Date date) {
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        return df.format(date);
+    }
+
+    public static String formattedNiceDate(Date date) {
+        DateFormat df = new SimpleDateFormat("dd MMM yyyy");
+        return df.format(date);
+    }
+
+    public static String getYear(Date date) {
+        DateFormat df = new SimpleDateFormat("yyyy");
+        return df.format(date);
+    }
+
+    public static boolean dateBefore(Date date, String dateStr) {
+        return date.before(parseDate(dateStr));
+    }
+
+    public static boolean dateOnOrAfter(Date date, String dateStr) {
+        Date refDate = parseDate(dateStr);
+        return date.equals(refDate) || date.after(refDate);
     }
 }
