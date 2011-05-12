29139d5b3c50f13ea86ddaba36fcc77d2a7e1506
natasha.carter@csiro.au <natasha.carter@csiro.au>
2011-05-12 00:13:07 +0000

    Added parser to convert verbatim longitude and latitude to decimal values. Only works with a very specific format as documented.
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/biocache-store@2196 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/scala/au/org/ala/biocache/parsers.scala
+++ src/main/scala/au/org/ala/biocache/parsers.scala
@@ -2,6 +2,62 @@ package au.org.ala.biocache
 
 import java.util.regex.Pattern
 
+
+
+object VerbatimLatLongParser {
+  
+  
+  val verbatimPattern = """([0-9]{1,3})(°[ ]*)([0-9]{1,2})?('[ ]*)?([0-9]{1,2}.?[0-9]{0,})?("[ ]*)?(s|south|n|north|w|west|e|east)""".r
+  val negativePattern = "(s|south|w|west)".r
+  
+  /**
+   * Parses the verbatim latutide or longitude
+   * Formats handled:
+   * 30° 01' S
+   * 153° 12' E
+   * 145° 44' 55.85" E
+   * 16° 52' 37" S
+   *
+   * TODO: Enhance this parser to cater for more formats and dirty data
+   *
+   */
+  def parse(stringValue:String) : Option[Float]={
+    try{
+        val normalised = stringValue.toLowerCase.trim
+      
+        normalised match{
+            case verbatimPattern(degree, dsign, minute, msign, second, ssign, direction) => convertToDecimal(degree, minute, second, direction)
+            case _ =>None
+        }
+      
+    } catch{
+        case e:Exception =>e.printStackTrace; None;
+    }
+    
+  }
+  def convertToDecimal(degree:String, minute:String, second:String, direction:String) : Option[Float] ={
+    var decimalValue = degree.toInt * 10000000
+    println("after degree: " + decimalValue)
+    if(minute != null)
+      decimalValue += ((minute.toInt*10000000)/60)
+    println("after minute: " + decimalValue)
+    if(second != null){
+      decimalValue += ((second.toFloat * 10000000).toInt/3600)
+    println("after second: "+ decimalValue + " - " + second + " = " + second.toFloat + " in degrees = " + (second.toFloat/3600))
+    }
+    try{
+      direction match{
+          case negativePattern(pat) => Some(-decimalValue.toFloat/10000000)
+          case _ => Some(decimalValue.toFloat/10000000)
+      }
+    }
+    catch{
+      case _ =>None
+    }
+
+    
+  }
+}
 object DistanceRangeParser {
 
    val singleNumber = """([0-9]{1,})""".r
