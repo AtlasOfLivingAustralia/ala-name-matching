4089f3ddcb560dbdb73a3f20809f610bba7295cf
peter.flemming@csiro.au <peter.flemming@csiro.au>
2011-05-23 07:29:38 +0000

    Refactored ValidationService
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/sensitive-species@2211 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/java/au/org/ala/sds/validation/ValidationService.java
+++ src/main/java/au/org/ala/sds/validation/ValidationService.java
@@ -1,9 +1,8 @@
 package au.org.ala.sds.validation;
 
-import au.org.ala.sds.model.SensitiveTaxon;
 
 public interface ValidationService {
 
-    ValidationOutcome validate(SensitiveTaxon sensitiveSpecies, FactCollection facts);
+    ValidationOutcome validate(FactCollection facts);
 
 }
