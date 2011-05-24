f258b2be874506c02f2c215319a5a849aa53690f
peter.flemming@csiro.au <peter.flemming@csiro.au>
2011-05-24 23:15:21 +0000

    New test
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/sensitive-species@2235 1ef1b718-b4f5-11dd-90a5-ed332e521da9

---
+++ src/test/java/au/org/ala/sds/ProblematicFileInputTest.java
@@ -0,0 +1,81 @@
+package au.org.ala.sds;
+
+import org.apache.commons.lang.StringUtils;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import au.org.ala.checklist.lucene.CBIndexSearch;
+import au.org.ala.sds.dao.DataRowHandler;
+import au.org.ala.sds.dao.DataStreamDao;
+import au.org.ala.sds.dao.DataStreamDaoFactory;
+import au.org.ala.sds.dto.DataColumnMapper;
+import au.org.ala.sds.dto.DataStreamProperties;
+import au.org.ala.sds.model.SensitiveTaxon;
+import au.org.ala.sds.validation.FactCollection;
+import au.org.ala.sds.validation.ServiceFactory;
+import au.org.ala.sds.validation.ValidationOutcome;
+import au.org.ala.sds.validation.ValidationService;
+
+public class ProblematicFileInputTest {
+
+    static CBIndexSearch cbIndexSearch;
+    static SensitiveSpeciesFinder finder;
+
+    @BeforeClass
+    public static void runOnce() throws Exception {
+        cbIndexSearch = new CBIndexSearch("/data/lucene/namematching");
+        finder = SensitiveSpeciesFinderFactory.getSensitiveSpeciesFinder("file:///data/sds/sensitive-species.xml", cbIndexSearch);
+    }
+
+    @Test
+    public void readExcelXmlWorkbook() throws Exception {
+        String inputFileName = "/Users/peterflemming/Dropbox/SDS/SDStest1.xls";
+        DataStreamDao dao = DataStreamDaoFactory.createDao(inputFileName);
+        DataColumnMapper mapper = new DataColumnMapper();
+        mapper.add("scientificName", "E");
+        mapper.add("decimalLatitude", "H");
+        mapper.add("decimalLongitude", "I");
+        mapper.add("eventDate", "K");
+        mapper.add("year", "K");
+
+        DataStreamProperties properties = new DataStreamProperties(2, 0, 1);
+
+        dao.processStream(
+                mapper,
+                properties,
+                new DataRowHandler() {
+                    public void handleRow(FactCollection facts) {
+                        processRow(facts);
+                    }
+                });
+    }
+
+    private void processRow(FactCollection facts) {
+        System.out.println("Row data - " + facts);
+        StringBuilder msgOut = new StringBuilder();
+        String name = facts.get(FactCollection.SCIENTIFIC_NAME_KEY);
+
+        SensitiveTaxon st = finder.findSensitiveSpecies(name);
+        if (st != null) {
+            ValidationService service = ServiceFactory.createValidationService(st);
+            ValidationOutcome outcome = service.validate(facts);
+
+            msgOut.append(st.getTaxonName());
+            if (st.getAcceptedName() != null && !name.equalsIgnoreCase(st.getAcceptedName())) {
+                msgOut.append("  (Matched on " + st.getAcceptedName() + ")");
+            }
+            if (StringUtils.isNotBlank(st.getCommonName())) {
+                msgOut.append(" [" + st.getCommonName() + "]");
+            }
+            if (StringUtils.isNotBlank(facts.get(FactCollection.ROW_KEY))) {
+                msgOut.append(" (row ").append(facts.get(FactCollection.ROW_KEY)).append(")");
+            }
+            msgOut.append("\n");
+
+        } else {
+            msgOut.append("Species '" + name + "' (row " + facts.get(FactCollection.ROW_KEY) + ") not found in list of sensitive species.\n");
+        }
+        System.out.print(msgOut.toString());
+    }
+
+}
