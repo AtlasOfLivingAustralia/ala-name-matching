4f9fc2fbb2b3ab3a1376cf05c942ca622faaddcd
djtfmartin <djtfmartin@gmail.com>
2011-07-04 01:24:26 +0000

    formatting
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/biocache-store@2325 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/scala/au/org/ala/util/FileHelper.scala
+++ src/main/scala/au/org/ala/util/FileHelper.scala
@@ -1,17 +1,19 @@
-/**************************************************************************
+/**
+ * ************************************************************************
  *  Copyright (C) 2010 Atlas of Living Australia
  *  All Rights Reserved.
- * 
+ *
  *  The contents of this file are subject to the Mozilla Public
  *  License Version 1.1 (the "License"); you may not use this file
  *  except in compliance with the License. You may obtain a copy of
  *  the License at http://www.mozilla.org/MPL/
- * 
+ *
  *  Software distributed under the License is distributed on an "AS
  *  IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
  *  implied. See the License for the specific language governing
  *  rights and limitations under the License.
- ***************************************************************************/
+ * *************************************************************************
+ */
 package au.org.ala.util
 import java.io._
 
@@ -20,74 +22,73 @@ import java.util.jar.JarFile
 /**
  * File helper - used as a implicit converter to add additional helper methods to java.io.File
  */
-class FileHelper(file : File) {
-  
-  def write(text : String) : Unit = {
-    val fw = new FileWriter(file)
-    try{ fw.write(text) }
-    finally{ fw.close }
-  }
-  
-  def foreachLine(proc : String=>Unit) : Unit = {
-    val br = new BufferedReader(new FileReader(file))
-    try{ 
-      while(br.ready){ 
-        proc(br.readLine)
-      }
+class FileHelper(file: File) {
+
+    def write(text: String): Unit = {
+        val fw = new FileWriter(file)
+        try { fw.write(text) }
+        finally { fw.close }
     }
-    finally{ br.close }
-  }
-  
-  def deleteAll : Unit = {
-    def deleteFile(dfile : File) : Unit = {
-      if(dfile.isDirectory){
-        val subfiles = dfile.listFiles
-        if(subfiles != null)
-          subfiles.foreach{ f => deleteFile(f) }
-      }
-      dfile.delete
+
+    def foreachLine(proc: String => Unit): Unit = {
+        val br = new BufferedReader(new FileReader(file))
+        try {
+            while (br.ready) {
+                proc(br.readLine)
+            }
+        } finally { br.close }
     }
-    deleteFile(file)
-  }
-  
-	def extractZip : Unit = {
-	  val basename = file.getName.substring(0, file.getName.lastIndexOf("."))
-	  val todir = new File(file.getParentFile, basename)
-	  todir.mkdirs()
-	      
-	  println("Extracting " + file + " to " + todir)
-	  val jar = new JarFile(file)
-	  val enu = jar.entries
-	  while(enu.hasMoreElements){
-	    val entry = enu.nextElement
-	    val entryPath = 
-	      if(entry.getName.startsWith(basename)) entry.getName.substring(basename.length) 
-	      else entry.getName
-	      
-	    println("Extracting to " + todir + "/" + entryPath)
-	    if(entry.isDirectory){
-	      new File(todir, entryPath).mkdirs
-	    }else{
-	      val istream = jar.getInputStream(entry)
-	      val ostream = new FileOutputStream(new File(todir, entryPath))
-	      copyStream(istream, ostream)
-	      ostream.close
-	      istream.close
-	     }
-	  }
-	}  
-    
-	private def copyStream(istream : InputStream, ostream : OutputStream) : Unit = {
-	  var bytes =  new Array[Byte](1024)
-	  var len = -1
-	  while({ len = istream.read(bytes, 0, 1024); len != -1 })
-	    ostream.write(bytes, 0, len)
-	}  
+
+    def deleteAll: Unit = {
+        def deleteFile(dfile: File): Unit = {
+            if (dfile.isDirectory) {
+                val subfiles = dfile.listFiles
+                if (subfiles != null)
+                    subfiles.foreach { f => deleteFile(f) }
+            }
+            dfile.delete
+        }
+        deleteFile(file)
+    }
+
+    def extractZip: Unit = {
+        val basename = file.getName.substring(0, file.getName.lastIndexOf("."))
+        val todir = new File(file.getParentFile, basename)
+        todir.mkdirs()
+
+        println("Extracting " + file + " to " + todir)
+        val jar = new JarFile(file)
+        val enu = jar.entries
+        while (enu.hasMoreElements) {
+            val entry = enu.nextElement
+            val entryPath =
+                if (entry.getName.startsWith(basename)) entry.getName.substring(basename.length)
+                else entry.getName
+
+            println("Extracting to " + todir + "/" + entryPath)
+            if (entry.isDirectory) {
+                new File(todir, entryPath).mkdirs
+            } else {
+                val istream = jar.getInputStream(entry)
+                val ostream = new FileOutputStream(new File(todir, entryPath))
+                copyStream(istream, ostream)
+                ostream.close
+                istream.close
+            }
+        }
+    }
+
+    private def copyStream(istream: InputStream, ostream: OutputStream): Unit = {
+        var bytes = new Array[Byte](1024)
+        var len = -1
+        while ({ len = istream.read(bytes, 0, 1024); len != -1 })
+            ostream.write(bytes, 0, len)
+    }
 }
 
 /**
  * Define a extensions to java.io.File
  */
-object FileHelper{
-  implicit def file2helper(file : File) = new FileHelper(file)
+object FileHelper {
+    implicit def file2helper(file: File) = new FileHelper(file)
 }
