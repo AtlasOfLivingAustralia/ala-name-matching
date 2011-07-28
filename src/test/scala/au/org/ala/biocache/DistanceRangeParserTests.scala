ff4251d47b3abd68011a497523d5eff117d996a9
djtfmartin <djtfmartin@gmail.com>
2011-07-28 11:31:50 +0000

    additional test
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/biocache-store@2435 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/test/scala/au/org/ala/biocache/DistanceRangeParserTests.scala
+++ src/test/scala/au/org/ala/biocache/DistanceRangeParserTests.scala
@@ -7,7 +7,7 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class DistanceRangeParserTests extends FunSuite {
 
-    test("Test range parse"){
+    test("Test range parsing"){
         expect(1.2222f) { DistanceRangeParser.parse("1.2222").get }
         expect(444f) { DistanceRangeParser.parse("444").get }
         expect(12000f) {DistanceRangeParser.parse("1-12km").get }
@@ -15,9 +15,9 @@ class DistanceRangeParserTests extends FunSuite {
         expect(10f) {DistanceRangeParser.parse("1m-10m").get }
         expect(10000f) {DistanceRangeParser.parse("1km-10km").get }
         expect(10500f) {DistanceRangeParser.parse("1km-10.5km").get }
-
         expect(10000f) {DistanceRangeParser.parse("> 10km").get }
         expect(10000f) {DistanceRangeParser.parse("< 10km").get }
-        //expect(11500f) {DistanceRangeParser.parse(">11.5km").get }
+        expect(11500f) {DistanceRangeParser.parse(">11.5km").get }
+        expect(11500f) {DistanceRangeParser.parse("10.2 - 11.5km").get }
     }
 }
