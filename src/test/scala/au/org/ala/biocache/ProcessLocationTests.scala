c968a6133dc5d2cd8483641640c9f4bdd62d3c5e
natasha.carter@csiro.au <natasha.carter@csiro.au>
2011-08-02 07:06:31 +0000

    New tests for not sensitive and already generalised.
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/biocache-store@2467 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/test/scala/au/org/ala/biocache/ProcessLocationTests.scala
+++ src/test/scala/au/org/ala/biocache/ProcessLocationTests.scala
@@ -20,6 +20,36 @@ class ProcessLocationTests extends FunSuite{
       LocationProcessor.process("test", raw, processed)
       expect("-35.2"){processed.location.decimalLatitude}
       expect("144.8"){processed.location.decimalLongitude}
+      expect(true){processed.occurrence.dataGeneralizations != null && processed.occurrence.dataGeneralizations.length>0}
+      
+  }
+  
+  test("Not Sensitive"){
+      val raw = new FullRecord
+      val processed = new FullRecord
+      processed.classification.setScientificName("Cataxia maculata")
+      processed.classification.setTaxonConceptID("urn:lsid:biodiversity.org.au:afd.taxon:41cc4a69-06d4-4591-9afe-7af431b7153c")
+      processed.classification.setTaxonRankID("7000")
+      raw.location.decimalLatitude = "-27.56"
+      raw.location.decimalLongitude = "152.28"
+      LocationProcessor.process("test", raw, processed)
+      expect("-27.56"){processed.location.decimalLatitude}
+      expect("152.28"){processed.location.decimalLongitude}
+      expect(true){processed.occurrence.dataGeneralizations == null}
+  }
+  
+  test("Already Generalised"){
+      val raw = new FullRecord
+      var processed = new FullRecord
+      processed.classification.setScientificName("Crex crex")
+      processed.classification.setTaxonConceptID("urn:lsid:biodiversity.org.au:afd.taxon:2ef4ac9c-7dfb-4447-8431-e337355ac1ca")
+      processed.classification.setTaxonRankID("7000")
+      raw.location.decimalLatitude = "-35.2"
+      raw.location.decimalLongitude = "144.8"
+      LocationProcessor.process("test", raw, processed)
+      expect("-35.2"){processed.location.decimalLatitude}
+      expect("144.8"){processed.location.decimalLongitude}
+      expect("Location is already generalised"){processed.occurrence.dataGeneralizations}
   }
   
   test("Uncertainty in Precision"){
