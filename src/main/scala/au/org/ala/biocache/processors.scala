b6c22375d7a04a0cd0f8938cfec1ec901e8463fd
natasha.carter@csiro.au <natasha.carter@csiro.au>
2011-08-03 01:25:04 +0000

    New SDS interface changes -prevents string comparisons of description.
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/biocache-store@2470 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/scala/au/org/ala/biocache/processors.scala
+++ src/main/scala/au/org/ala/biocache/processors.scala
@@ -374,11 +374,11 @@ object LocationProcessor extends Processor {
                     if(voutcome.isValid && voutcome.isInstanceOf[ConservationOutcome]){
                        val outcome =voutcome.asInstanceOf[ConservationOutcome]
                        val gl = outcome.getGeneralisedLocation
-                       if(gl.getDescription.startsWith("Location not generalised since")){                           
+                       if(!gl.isSensitive){                           
                            //don't generalise since it is not part of the Location where it should be generalised
-                           //TODO Talk to Peter about a better method to determine this...
+                          
                        }
-                       else if(gl.getDescription == MessageFactory.getMessageText(MessageFactory.LOCATION_ALREADY_GENERALISED)){
+                       else if(!gl.isGeneralised){
                          //already been genaralised by data resource provider non need to do anything.
                            processed.occurrence.dataGeneralizations = gl.getDescription
                        }
