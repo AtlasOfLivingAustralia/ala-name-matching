fc8d1805fe1c6200a4a6684f561487fb09e1cda8
tommywang1981@gmail.com <tommywang1981@gmail.com>
2011-08-16 01:01:34 +0000

    Unit test for ClassificationProcessor
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/biocache-store@2561 1ef1b718-b4f5-11dd-90a5-ed332e521da9

---
+++ src/test/scala/au/org/ala/biocache/TaxonomicNameTest.scala
@@ -0,0 +1,39 @@
+package au.org.ala.biocache
+
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+import org.junit.runner.RunWith
+
+@RunWith(classOf[JUnitRunner])
+class TaxonomicNameTest extends FunSuite {
+
+    test("name not recognised"){
+        val raw = new FullRecord
+        var processed = new FullRecord
+        raw.classification.scientificName = "dummy name"
+        val qas = (new ClassificationProcessor).process("test", raw, processed)
+        expect(10004){qas(0).code}
+    }
+    
+    test("name not in national checklists"){
+        val raw = new FullRecord
+        var processed = new FullRecord
+        raw.classification.scientificName = "Amanita farinacea"
+        var qas = (new ClassificationProcessor).process("test", raw, processed)
+        expect(true){qas.isEmpty}
+        
+        raw.classification.scientificName = "Camponotus reticulatus fullawayi"
+        qas = (new ClassificationProcessor).process("test", raw, processed)
+        expect(10005){qas(0).code}
+    }
+    
+    test("homonym issue"){
+        val raw = new FullRecord
+        var processed = new FullRecord
+        raw.classification.genus = "Heteropoda";
+        raw.classification.scientificName = "Heteropoda";
+        val qas = (new ClassificationProcessor).process("test", raw, processed);
+        println(processed.classification.taxonConceptID)
+        expect(10006){qas(0).code}
+    }
+}
