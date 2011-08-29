be1b26db17940c3796ebfac9ff3264ab70d7661e
dcwmk282@gmail.com <dcwmk282@gmail.com>
2011-08-29 01:04:15 +0000

    added solr boost value for bie-search
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/ala-name-matching@2631 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/java/au/org/ala/data/util/RankType.java
+++ src/main/java/au/org/ala/data/util/RankType.java
@@ -17,48 +17,48 @@ import org.gbif.ecat.voc.Rank;
  */
 public enum RankType {
 
-    KINGDOM(1000, "kingdom", Rank.KINGDOM),
-    SUBKINGDOM(1200, "subkingdom", Rank.Subkingdom),
-    PHYLUM(2000, "phylum", Rank.PHYLUM),
-    SUBPHYLUM(2200, "subphylum", Rank.Subphylum),
-    SUPERCLASS(2800, "superclass", Rank.Superclass),
-    CLASS(3000, "class", Rank.CLASS),
-    SUBCLASS(3200, "subclass", Rank.Subclass),
-    SUPERORDER(3800, "superorder", Rank.Superorder),
-    ORDER(4000, "order", Rank.ORDER),
-    SUBORDER(4200, "suborder", Rank.Suborder),
-    INFRAORDER(4350, "infraorder", Rank.Infraorder),
-    SUPERFAMILY(4500, "superfamily", Rank.Superfamily),
-    FAMILY(5000, "family", Rank.FAMILY),
-    SUBFAMILY(5500, "subfamily", Rank.Subfamily),
-    TRIBE(5600, "tribe", Rank.Tribe),
-    SUBTRIBE(5700, "subtribe", Rank.Subtribe),
-    GENUS(6000, "genus", Rank.GENUS),
-    SUBGENUS(6500, "subgenus", Rank.SUBGENUS),
-    SECTION(6600, "section", Rank.Section),
-    SUBSECTION(6700, "subsection", Rank.Subsection),
-    SERIES(6800, "series", Rank.Series),
-    SUBSERIES(6900, "subseries", Rank.Subseries),
-    INFRAGENERICNNAME(6925, "infragenericname", Rank.InfragenericName),
-    SPECIES(7000, "species", Rank.SPECIES),
-    INFRASPECIFICNAME(8090, "infraspecificname", Rank.InfraspecificName),
-    SUBSPECIES(8000, "subspecies", Rank.SUBSPECIES),
-    INFRASUBSPECIESNAME(-1, "infrasubspeciesname", Rank.InfrasubspecificName),
-    VARIETY(8010, "variety", Rank.VARIETY),
-    SUBVARIETY(8015, "subvariety", Rank.Subvariety),
-    FORM(8020, "form", Rank.Form),
-    SUBFORM(-1, "subform", Rank.Subform),
-    CULTIVARGROUP(-1, "cultivargroup", Rank.CultivarGroup),
-    CULTIVAR(8050, "cultivar", Rank.Cultivar),
-    INFORMAL(-1, "informal", Rank.Informal),
-    UNRANKED(0, "unranked", Rank.Unranked),
-    SUPRAGENERICNAME(8200, "supragenericname", Rank.SupragenericName),
-    HYBRID(8150, "hybrid",null);
+    KINGDOM(1000, "kingdom", Rank.KINGDOM, 2f),
+    SUBKINGDOM(1200, "subkingdom", Rank.Subkingdom, null),
+    PHYLUM(2000, "phylum", Rank.PHYLUM, 2f),
+    SUBPHYLUM(2200, "subphylum", Rank.Subphylum, null),
+    SUPERCLASS(2800, "superclass", Rank.Superclass, null),
+    CLASS(3000, "class", Rank.CLASS, 2f),
+    SUBCLASS(3200, "subclass", Rank.Subclass, null),
+    SUPERORDER(3800, "superorder", Rank.Superorder, null),
+    ORDER(4000, "order", Rank.ORDER, 2f),
+    SUBORDER(4200, "suborder", Rank.Suborder, null),
+    INFRAORDER(4350, "infraorder", Rank.Infraorder, null),
+    SUPERFAMILY(4500, "superfamily", Rank.Superfamily, null),
+    FAMILY(5000, "family", Rank.FAMILY, 2f),
+    SUBFAMILY(5500, "subfamily", Rank.Subfamily, null),
+    TRIBE(5600, "tribe", Rank.Tribe, null),
+    SUBTRIBE(5700, "subtribe", Rank.Subtribe, null),
+    GENUS(6000, "genus", Rank.GENUS, 3f),
+    SUBGENUS(6500, "subgenus", Rank.SUBGENUS, null),
+    SECTION(6600, "section", Rank.Section, null),
+    SUBSECTION(6700, "subsection", Rank.Subsection, null),
+    SERIES(6800, "series", Rank.Series, null),
+    SUBSERIES(6900, "subseries", Rank.Subseries, null),
+    INFRAGENERICNNAME(6925, "infragenericname", Rank.InfragenericName, null),
+    SPECIES(7000, "species", Rank.SPECIES, 2f),
+    INFRASPECIFICNAME(8090, "infraspecificname", Rank.InfraspecificName, null),
+    SUBSPECIES(8000, "subspecies", Rank.SUBSPECIES, null),
+    INFRASUBSPECIESNAME(-1, "infrasubspeciesname", Rank.InfrasubspecificName, null),
+    VARIETY(8010, "variety", Rank.VARIETY, null),
+    SUBVARIETY(8015, "subvariety", Rank.Subvariety, null),
+    FORM(8020, "form", Rank.Form, null),
+    SUBFORM(-1, "subform", Rank.Subform, null),
+    CULTIVARGROUP(-1, "cultivargroup", Rank.CultivarGroup, null),
+    CULTIVAR(8050, "cultivar", Rank.Cultivar, null),
+    INFORMAL(-1, "informal", Rank.Informal, null),
+    UNRANKED(0, "unranked", Rank.Unranked, null),
+    SUPRAGENERICNAME(8200, "supragenericname", Rank.SupragenericName, null),
+    HYBRID(8150, "hybrid",null, null);
     // Allow reverse-lookup (based on http://www.ajaxonomy.com/2007/java/making-the-most-of-java-50-enum-tricks)
     private static final Map<String, RankType> fieldLookup = new HashMap<String, RankType>();
     private static final Map<Integer, RankType> idLookup = new HashMap<Integer, RankType>();
     private static final Map<Rank, RankType> cbRankLookup = new HashMap<Rank, RankType>();
-
+    
     static {
         for (RankType rt : EnumSet.allOf(RankType.class)) {
             fieldLookup.put(rt.getRank(), rt);
@@ -70,11 +70,13 @@ public enum RankType {
     private Integer id;
     private String field;
     private Rank cbRank;
-
-    private RankType(Integer id, String field, Rank rank) {
+    private Float boost;
+    
+	private RankType(Integer id, String field, Rank rank, Float boost) {
         this.id = id;
         this.field = field;
         this.cbRank = rank;
+        this.boost = boost;
     }
     public static Set<RankType> getAllRanksBelow(Integer rank){
         Set<RankType> ranks = new TreeSet<RankType>();
@@ -99,6 +101,10 @@ public enum RankType {
         return this.field;
     }
 
+    public Float getBoost() {
+		return boost;
+	}
+    
     /**
      * @param field
      * @return The rankType for the field
