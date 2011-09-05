fdcd5ba4a49c61470f43bb29e8e19bfddc3fa9c2
peter.flemming@csiro.au <peter.flemming@csiro.au>
2011-09-05 05:27:12 +0000

    Added getSensitiveSpeciesFinder(CBIndexSearch) so client doesn't need to know about location of sensitive-species.xml
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/sensitive-species@2657 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/java/au/org/ala/sds/SensitiveSpeciesFinderFactory.java
+++ src/main/java/au/org/ala/sds/SensitiveSpeciesFinderFactory.java
@@ -3,6 +3,8 @@
  */
 package au.org.ala.sds;
 
+import java.io.IOException;
+
 import javax.sql.DataSource;
 
 import au.org.ala.checklist.lucene.CBIndexSearch;
@@ -27,10 +29,28 @@ public class SensitiveSpeciesFinderFactory {
 
     public static SensitiveSpeciesFinder getSensitiveSpeciesFinder(String url, CBIndexSearch cbIndexSearcher) throws Exception {
 
-        SensitiveSpeciesDao dao = new SensitiveSpeciesXmlDao(url);
-        SensitiveTaxonStore store = new SensitiveTaxonStore(dao, cbIndexSearcher);
+        SensitiveSpeciesDao dao = null;
+        SensitiveTaxonStore store = null;
+
+        try {
+            dao = new SensitiveSpeciesXmlDao(url);
+            store = new SensitiveTaxonStore(dao, cbIndexSearcher);
+        } catch (Exception e) {
+            if (e instanceof IOException) {
+                try {
+
+                } catch (Exception e1) {
+
+                }
+            }
+        }
         return new SensitiveSpeciesFinder(store);
 
     }
 
+    public static SensitiveSpeciesFinder getSensitiveSpeciesFinder(CBIndexSearch cbIndexSearcher) throws Exception {
+
+        return getSensitiveSpeciesFinder("http://sds.ala.org.au/sensitive-species-data.xml", cbIndexSearcher);
+
+    }
 }
