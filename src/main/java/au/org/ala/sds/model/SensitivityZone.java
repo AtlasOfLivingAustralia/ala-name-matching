c3d0b14abdb2d9e028c4d1dbf96af91850f39b23
peter.flemming@csiro.au <peter.flemming@csiro.au>
2011-09-05 05:30:47 +0000

    Remove "in" from string returned from getState()
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/sensitive-species@2659 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/java/au/org/ala/sds/model/SensitivityZone.java
+++ src/main/java/au/org/ala/sds/model/SensitivityZone.java
@@ -156,7 +156,7 @@ public class SensitivityZone {
     public static String getState(List<SensitivityZone> zones) {
         for (SensitivityZone sz : zones) {
             if (sz.getType().equals(ZoneType.STATE)) {
-                return "in " + sz.getId();
+                return sz.getId();
             } else if (sz.getId().equals(NOTAUS)) {
                 return sz.getName();
             }
