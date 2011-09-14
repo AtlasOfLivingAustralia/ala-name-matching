691d14abf17b647c0cd502364322ba6705cda4e9
natasha.carter@csiro.au <natasha.carter@csiro.au>
2011-09-14 04:45:47 +0000

    Fixes to geospatiallyKosher in index ONLY.
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/biocache-store@2719 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/scala/au/org/ala/util/ReprocessIndexSelect.scala
+++ src/main/scala/au/org/ala/util/ReprocessIndexSelect.scala
@@ -12,24 +12,27 @@ object ReprocessIndexSelect {
         var query:Option[String]=None;
         var threads = 4
         var exist = false;
+        var indexOnly =false
         var startUuid:Option[String]=None
         val parser = new OptionParser("index records options") {
             opt("q", "query", "The query to run e.g. 'year:[2001 TO *]' or 'taxon_name:Macropus'", { v:String => query = Some(v) })
             intOpt("t", "thread", "The number of threads to use", {v:Int => threads = v } )
             opt("exist", "use the existing list of row keys", {exist=true})
             opt("s", "start","The record to start processing with", {v:String => startUuid = Some(v)})
+            opt("index", "reindex only - do not reprocess", {indexOnly=true})
         }
 
         if (parser.parse(args)) {
             if(!query.isEmpty){
-              reprocessindex(query.get, threads, exist, startUuid)
+                reprocessindex(query.get, threads, exist, startUuid, indexOnly)
+
             } else {
               parser.showUsage
             }
         }
     }
 
-    def reprocessindex(query: String, threads: Int, exist: Boolean, startUuid:Option[String]) {
+    def reprocessindex(query: String, threads: Int, exist: Boolean, startUuid:Option[String], indexOnly:Boolean) {
         //get the list of rowKeys to be processed.
         val indexer = Config.getInstance(classOf[IndexDAO]).asInstanceOf[IndexDAO]
 
@@ -40,7 +43,8 @@ object ReprocessIndexSelect {
             out.flush()
             out.close()
         }
-        ProcessWithActors.processRecords(threads, file, startUuid)
+        if(!indexOnly)
+            ProcessWithActors.processRecords(threads, file, startUuid)
         IndexRecords.indexList(file)
     }
 }
