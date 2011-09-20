7e7fd9f1d2e0cff0de3c9cf2ba11c6c15c0b3d2d
dcwmk282@gmail.com <dcwmk282@gmail.com>
2011-09-20 06:10:39 +0000

    temperory remove/comment out the code that retrieves the "associatedMedia" from the message in the JMS queue.
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/biocache-jms@2770 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/java/org/ala/jms/dto/CitizenScience.java
+++ src/main/java/org/ala/jms/dto/CitizenScience.java
@@ -6,10 +6,19 @@ public class CitizenScience extends Sighting {
 	String[] associatedMedia;
 
 	public String[] getAssociatedMedia() {
-		return associatedMedia;
+/**
+ * Date: 20 Sept 2010.
+ * temperory remove/comment out the code that retrieves the "associatedMedia" from the message in the JMS queue.		
+ */
+//		return this.associatedMedia;
+		return  new String[]{};
 	}
 
 	public void setAssociatedMedia(String[] associatedMedia) {
-		this.associatedMedia = associatedMedia;
+		/**
+		 * Date: 20 Sept 2010.
+		 * temperory remove/comment out the code that retrieves the "associatedMedia" from the message in the JMS queue.		
+		 */		
+//		this.associatedMedia = associatedMedia;
 	}
 }
