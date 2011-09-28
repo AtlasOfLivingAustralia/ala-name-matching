8aaeb97454cf6c9436247764df66e569deb43a97
djtfmartin <djtfmartin@gmail.com>
2011-09-28 06:46:55 +0000

    commandlinetool enhancements
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/biocache-store@2787 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/scala/au/org/ala/util/CommandLineTool.scala
+++ src/main/scala/au/org/ala/util/CommandLineTool.scala
@@ -1,104 +1,118 @@
 package au.org.ala.util
 
+import au.org.ala.biocache.Config
+import org.codehaus.jackson.map.ObjectMapper
+
+/**
+ * Command line tool that allows administrators to run commands on
+ * the biocache.
+ */
 object CommandLineTool {
 
-    def main(args: Array[String]) {
+  def main(args: Array[String]) {
+    println("----------------------------")
+    println("- Biocache management tool -")
+    println("----------------------------")
+    print("\nPlease supply a command or hit ENTER to view command list:")
+    var input = readLine
 
-        println("Welcome to the biocache management tool.")
-        print("Please supply a command or hit ENTER to view command list:")
-        var input = readLine
+    val l = new Loader
+    while (input != "exit" && input != "q" && input != "quit") {
 
-        val l = new Loader
-        while (input != "exit" && input != "q" && input != "quit") {
-
-            try {
-                input.toLowerCase.trim match {
-                    case it if (it startsWith "describe ") || (it startsWith "d ") => l.describeResource(it.split(" ").map(x => x.trim).toList.tail)
-                    case it if (it startsWith "list") || (it == "l") => l.printResourceList
-                    case it if (it startsWith "load") || (it startsWith "ld") => l.load(it.split(" ").map(x => x.trim).toList.last)
-                    case it if (it startsWith "process") || (it startsWith "process") => {
-                        val drs = it.split(" ").map(x => x.trim).toList.tail
-                        for(dr <- drs){
-                        	ProcessWithActors.processRecords(4, None, Some(dr))
-                        }
-                    }
-                    case it if (it startsWith "index") || (it startsWith "index") => {
-                        val drs = it.split(" ").map(x => x.trim).toList.tail
-                        for(dr <- drs){
-                        	IndexRecords.index(None, Some(dr), false, false)
-                        }
-                    }
-                    case it if (it startsWith "createdwc")=> {
-                        val args = it.split(" ").map(x => x.trim).toArray.tail
-                        DwCACreator.main(args)
-                    }
-                    case it if (it startsWith "optimise") => {
-                       	IndexRecords.indexer.optimise
-                    }
-                    case it if (it startsWith "healthcheck") => l.healthcheck
-                    case it if (it startsWith "export") => {
-                        val args = it.split(" ").map(x => x.trim).toArray.tail
-                        ExportUtil.main(args)
-                    }
-                    case it if (it startsWith "import") => {
-                        val args = it.split(" ").map(x => x.trim).toArray.tail
-                        ImportUtil.main(args)
-                    }
-                    case _ => printHelp
-                }
-            } catch {
-                case e: Exception => e.printStackTrace
+      try {
+        input.toLowerCase.trim match {
+          case it if (it startsWith "describe ") || (it startsWith "d ") => l.describeResource(it.split(" ").map(x => x.trim).toList.tail)
+          case it if (it startsWith "list") || (it == "l") => l.printResourceList
+          case it if (it startsWith "load") || (it startsWith "ld") => l.load(it.split(" ").map(x => x.trim).toList.last)
+          case it if (it startsWith "process single") => {
+            ProcessSingleRecord.processRecord(it.split(" ").map(x => x.trim).toList.last)
+          }
+          case it if (it startsWith "process") || (it startsWith "process") => {
+            val drs = it.split(" ").map(x => x.trim).toList.tail
+            for (dr <- drs) {
+              ProcessWithActors.processRecords(4, None, Some(dr))
             }
-            print("\nPlease supply a command or hit ENTER to view command list:")
-            input = readLine
+          }
+          case it if (it startsWith "index") || (it startsWith "index") => {
+            val drs = it.split(" ").map(x => x.trim).toList.tail
+            for (dr <- drs) {
+              IndexRecords.index(None, Some(dr), false, false)
+            }
+          }
+          case it if (it startsWith "createdwc") => {
+            val args = it.split(" ").map(x => x.trim).toArray.tail
+            DwCACreator.main(args)
+          }
+          case it if (it startsWith "optimise") => {
+            IndexRecords.indexer.optimise
+          }
+          case it if (it startsWith "healthcheck") => l.healthcheck
+          case it if (it startsWith "export") => {
+            val args = it.split(" ").map(x => x.trim).toArray.tail
+            ExportUtil.main(args)
+          }
+          case it if (it startsWith "import") => {
+            val args = it.split(" ").map(x => x.trim).toArray.tail
+            ImportUtil.main(args)
+          }
+          case _ => printHelp
         }
-        //close down the data store and index so the program can exit normally
-        au.org.ala.biocache.Config.persistenceManager.shutdown
-        IndexRecords.indexer.shutdown
-        println("Goodbye\n")
+      } catch {
+        case e: Exception => e.printStackTrace
+      }
+      print("\nPlease supply a command or hit ENTER to view command list:")
+      input = readLine
     }
+    //close down the data store and index so the program can exit normally
+    au.org.ala.biocache.Config.persistenceManager.shutdown
+    IndexRecords.indexer.shutdown
+    println("Goodbye\n")
+  }
 
-    def printHelp = {
-        println("1)  list - print list of resources")
-        println("2)  describe <dr-uid> <dr-uid1> <dr-uid2>... - print list of resources")
-        println("3)  load <dr-uid> - load resource")
-        println("4)  process <dr-uid> - process resource")
-        println("5)  index <dr-uid> - index resource")
-        println("6)  createdwc <dr-uid> <export directory>")
-        println("7)  healthcheck")
-        println("8)  export")
-        println("9)  import")
-        println("10)  optimise")
-        println("11)  exit")
-    }
+  def printHelp = {
+    println("1)  list - print list of resources")
+    println("2)  describe <dr-uid> <dr-uid1> <dr-uid2>... - print list of resources")
+    println("3)  load <dr-uid> - load resource")
+    println("4)  process single <uuid> - process single record")
+    println("5)  process <dr-uid> - process resource")
+    println("6)  index <dr-uid> - index resource")
+    println("7)  createdwc <dr-uid> <export directory>")
+    println("8)  healthcheck")
+    println("9)  export")
+    println("10)  import")
+    println("11)  optimise")
+    println("12)  exit")
+  }
 
-    def printTable(table: List[Map[String, String]]) {
+  def printTable(table: List[Map[String, String]]) {
 
-        val keys = table(0).keys.toList
-        val valueLengths = keys.map(k => { (k, table.map(x => x(k).length).max) }).toMap[String, Int]
-        val columns = table(0).keys.map(k => {
-            if (k.length < valueLengths(k)) {
-                k + (List.fill[String](valueLengths(k) - k.length)(" ").mkString)
-            } else {
-                k
-            }
-        }).mkString(" | ", " | ", " |")
+    val keys = table(0).keys.toList
+    val valueLengths = keys.map(k => {
+      (k, table.map(x => x(k).length).max)
+    }).toMap[String, Int]
+    val columns = table(0).keys.map(k => {
+      if (k.length < valueLengths(k)) {
+        k + (List.fill[String](valueLengths(k) - k.length)(" ").mkString)
+      } else {
+        k
+      }
+    }).mkString(" | ", " | ", " |")
 
-        val sep = " " + List.fill[String](columns.length - 1)("-").mkString
-        println(sep)
-        println(columns)
-        println(" |" + List.fill[String](columns.length - 3)("-").mkString + "|")
+    val sep = " " + List.fill[String](columns.length - 1)("-").mkString
+    println(sep)
+    println(columns)
+    println(" |" + List.fill[String](columns.length - 3)("-").mkString + "|")
 
-        table.foreach(dr => {
-            println(dr.map(kv => {
-                if (kv._2.length < valueLengths(kv._1)) {
-                    kv._2 + (List.fill[String](valueLengths(kv._1) - kv._2.length)(" ").mkString)
-                } else {
-                    kv._2
-                }
-            }).mkString(" | ", " | ", " |"))
-        })
+    table.foreach(dr => {
+      println(dr.map(kv => {
+        if (kv._2.length < valueLengths(kv._1)) {
+          kv._2 + (List.fill[String](valueLengths(kv._1) - kv._2.length)(" ").mkString)
+        } else {
+          kv._2
+        }
+      }).mkString(" | ", " | ", " |"))
+    })
 
-        println(" " + List.fill[String](columns.length - 1)("-").mkString)
-    }
+    println(" " + List.fill[String](columns.length - 1)("-").mkString)
+  }
 }
--- src/main/scala/au/org/ala/util/ProcessSingleRecord.scala
+++ src/main/scala/au/org/ala/util/ProcessSingleRecord.scala
@@ -1,39 +1,46 @@
 package au.org.ala.util
+
 import au.org.ala.biocache.Config
 import java.io.{InputStreamReader, BufferedReader}
 import org.codehaus.jackson.map.ObjectMapper
 
+/**
+ * Utility for processing a single record.
+ */
 object ProcessSingleRecord {
 
-    def main(args:Array[String]){
-
-        val processor = new RecordProcessor
-
-        print("Supply a UUID or a Row Key for a record: ")
-        var uuid = readStdIn
-        while(uuid != "q" && uuid !="exit"){
-            var rawRecord = Config.occurrenceDAO.getByRowKey(uuid, au.org.ala.biocache.Raw)
-            if(rawRecord.isEmpty){
-            	rawRecord = Config.occurrenceDAO.getByUuid(uuid, au.org.ala.biocache.Raw)
-            }
-            if(!rawRecord.isEmpty) {
-               println("Processing record.....")
-               processor.processRecordAndUpdate(rawRecord.get)
-               val processedRecord = Config.occurrenceDAO.getByRowKey(rawRecord.get.rowKey, au.org.ala.biocache.Processed)
-               val objectMapper = new ObjectMapper
-               if(!processedRecord.isEmpty)
-                  println(objectMapper.writeValueAsString(processedRecord.get))
-               else
-                  println("Record not found")
-            } else {
-            	println("UUID or row key not stored....")
-            }
-            print("\n\nSupply a Row Key for a record: ")
-            uuid = readStdIn
-        }
-        println("Exiting...")
-        exit(1)
+  def processRecord(uuid: String) {
+    val processor = new RecordProcessor
+    var rawRecord = Config.occurrenceDAO.getByRowKey(uuid, au.org.ala.biocache.Raw)
+    if (rawRecord.isEmpty) {
+      rawRecord = Config.occurrenceDAO.getByUuid(uuid, au.org.ala.biocache.Raw)
     }
+    if (!rawRecord.isEmpty) {
+      println("Processing record.....")
+      processor.processRecordAndUpdate(rawRecord.get)
+      val processedRecord = Config.occurrenceDAO.getByRowKey(rawRecord.get.rowKey, au.org.ala.biocache.Processed)
+      val objectMapper = new ObjectMapper
+      if (!processedRecord.isEmpty)
+        println(objectMapper.writeValueAsString(processedRecord.get))
+      else
+        println("Record not found")
+    } else {
+      println("UUID or row key not stored....")
+    }
+    print("\n\nSupply a Row Key for a record: ")
+  }
 
-    def readStdIn = (new BufferedReader(new InputStreamReader(System.in))).readLine.trim
+  def main(args: Array[String]) {
+
+    print("Supply a UUID or a Row Key for a record: ")
+    var uuid = readStdIn
+    while (uuid != "q" && uuid != "exit") {
+      processRecord(uuid)
+      uuid = readStdIn
+    }
+    println("Exiting...")
+    exit(1)
+  }
+
+  def readStdIn = (new BufferedReader(new InputStreamReader(System.in))).readLine.trim
 }
