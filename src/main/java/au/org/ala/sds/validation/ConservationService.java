46f504bb33a8b28e2a4657f1bbfede42136370f6
natasha.carter@csiro.au <natasha.carter@csiro.au>
2011-10-03 23:53:29 +0000

    Added some more sensitive fields. Included the rules in the daatGeneralizations.
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/sensitive-species@2810 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/java/au/org/ala/sds/validation/ConservationService.java
+++ src/main/java/au/org/ala/sds/validation/ConservationService.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Map;
 
 import au.org.ala.sds.model.SensitiveTaxon;
+import au.org.ala.sds.model.SensitivityInstance;
 import au.org.ala.sds.model.SensitivityZone;
 import au.org.ala.sds.util.GeneralisedLocation;
 import au.org.ala.sds.util.GeneralisedLocationFactory;
@@ -57,12 +58,28 @@ public class ConservationService implements ValidationService {
         Map<String, Object> results = new HashMap<String, Object>();
         Map<String, String> originalSensitiveValues = new HashMap<String, String>();
         if (gl.isSensitive()) {
+            StringBuilder extra = new StringBuilder();
+            for(SensitivityInstance si :gl.getSensitivityInstances()){
+                if(extra.length()>0)
+                    extra.append("\t");
+                else
+                    extra.append("\n");
+                extra.append("Sensitive in ")
+                      .append(si.getZone())
+                      .append(" [")
+                      .append(si.getCategory().getValue())
+                      .append(", ").append(si.getAuthority()).append("]");
+            }
+            String extraDesc = extra.toString();
             outcome.setSensitive(true);
             results.put(FactCollection.DECIMAL_LATITUDE_KEY, gl.getGeneralisedLatitude());
             results.put(FactCollection.DECIMAL_LONGITUDE_KEY, gl.getGeneralisedLongitude());
             results.put("generalisationInMetres", gl.getGeneralisationInMetres());
-            results.put("dataGeneralizations", gl.getDescription());
-            if (gl.getGeneralisationInMetres().equals("")) {
+            results.put("dataGeneralizations", gl.getDescription() + extraDesc);
+            emptyValueIfNecessary("locationRemarks", biocacheData, originalSensitiveValues, results);
+            emptyValueIfNecessary("verbatimLatitude", biocacheData, originalSensitiveValues, results);
+            emptyValueIfNecessary("verbatimLongitude", biocacheData, originalSensitiveValues, results);
+            if (gl.getGeneralisationInMetres().equals("") && gl.getGeneralisedLatitude().equals("")) {
                 results.put("informationWithheld", "The location has been withheld in accordance with " + facts.get(FactCollection.STATE_PROVINCE_KEY) + " sensitive species policy");
             }
             originalSensitiveValues.put(FactCollection.DECIMAL_LATITUDE_KEY, gl.getOriginalLatitude());
@@ -73,13 +90,16 @@ public class ConservationService implements ValidationService {
 
         // Handle Birds Australia occurrences
         if (facts.get("dataResourceUid") != null && facts.get("dataResourceUid").equalsIgnoreCase("dr359")) {
-            results.put("eventID", "");
-            results.put("locationRemarks", "");
-            results.put("day", "");
+//            results.put("eventID", "");
+//            results.put("locationRemarks", "");
+//            results.put("day", "");
+            emptyValueIfNecessary("eventID", biocacheData, originalSensitiveValues, results);
+            emptyValueIfNecessary("day", biocacheData, originalSensitiveValues, results);
+            emptyValueIfNecessary("eventDate",biocacheData, originalSensitiveValues, results);
             results.put("informationWithheld", "The eventID and day information has been withheld in accordance with Birds Australia data policy");
-            originalSensitiveValues.put("eventID", facts.get("eventID"));
-            originalSensitiveValues.put("locationRemarks", facts.get("locationRemarks"));
-            originalSensitiveValues.put("day", facts.get("day"));
+//            originalSensitiveValues.put("eventID", facts.get("eventID"));
+//            originalSensitiveValues.put("locationRemarks", facts.get("locationRemarks"));
+//            originalSensitiveValues.put("day", facts.get("day"));
         }
 
         results.put("originalSensitiveValues", originalSensitiveValues);
@@ -87,6 +107,12 @@ public class ConservationService implements ValidationService {
         outcome.setValid(true);
 
         return outcome;
+    }
+    private void emptyValueIfNecessary(String field, Map<String,String> facts, Map<String,String> originalSensitiveValues, Map<String,Object> results){
+        if(facts.containsKey(field)){
+            results.put(field, "");
+            originalSensitiveValues.put(field, facts.get(field));
+        }
     }
 
     public void setReportFactory(ReportFactory reportFactory) {
