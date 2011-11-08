6288c79ce51c3a322fb522351378576192c95c7b
peter.flemming@csiro.au <peter.flemming@csiro.au>
2011-11-08 03:10:06 +0000

    Implement Serializable.
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/sensitive-species@2852 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/java/au/org/ala/sds/model/ConservationInstance.java
+++ src/main/java/au/org/ala/sds/model/ConservationInstance.java
@@ -3,11 +3,15 @@
  */
 package au.org.ala.sds.model;
 
+import java.io.Serializable;
+
 /**
  *
  * @author Peter Flemming (peter.flemming@csiro.au)
  */
-public class ConservationInstance extends SensitivityInstance {
+public class ConservationInstance extends SensitivityInstance implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     private final String locationGeneralisation;
 
