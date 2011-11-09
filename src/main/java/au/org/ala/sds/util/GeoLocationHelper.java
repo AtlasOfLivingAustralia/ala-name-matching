6f2deacf29365a8e2412831aab29c926fbe23855
peter.flemming@csiro.au <peter.flemming@csiro.au>
2011-11-09 06:48:14 +0000

    Changes for new Spatial layers web service API - JSON response instead of XML.
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/sensitive-species@2867 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/java/au/org/ala/sds/util/GeoLocationHelper.java
+++ src/main/java/au/org/ala/sds/util/GeoLocationHelper.java
@@ -4,89 +4,78 @@ import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-import java.util.zip.GZIPInputStream;
 
 import org.apache.commons.lang.math.NumberUtils;
 import org.apache.log4j.Logger;
-import org.jdom.Document;
-import org.jdom.Element;
-import org.jdom.input.SAXBuilder;
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
 
 import au.org.ala.sds.model.SensitivityZone;
 import au.org.ala.sds.model.SensitivityZoneFactory;
 
 public class GeoLocationHelper {
 
-    @SuppressWarnings("unchecked")
     public static Set<SensitivityZone> getZonesContainingPoint(String latitude, String longitude) throws Exception {
 
         final Logger logger = Logger.getLogger(GeoLocationHelper.class);
+
+        final String COASTAL_WATERS_LAYER = "cl927";
+        final String LGA_BOUNDARIES_LAYER = "cl23";
+
         Set<SensitivityZone> zones = new HashSet<SensitivityZone>();;
 
         //
         // Call geospatial web service
         //
-        URL url = new URL("http://spatial.ala.org.au/gazetteer/latlon/" + latitude + "," + longitude);
+        URL url = new URL(Configuration.getInstance().getSpatialUrl() + latitude + "/" + longitude);
         URLConnection connection = url.openConnection();
-        connection.setRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
-        connection.setRequestProperty("Accept-Encoding", "gzip,deflate");
         logger.debug("Looking up location using " + url);
         InputStream inStream = connection.getInputStream();
-        GZIPInputStream gis = new GZIPInputStream(inStream);
 
         //
-        // Parse XML result
+        // Parse JSON result
         //
         logger.debug("Parsing location results");
-        SAXBuilder parser = new SAXBuilder();
-        Document doc = parser.build(gis);
-        Element root = doc.getRootElement();
-        List<Element> results = root.getChild("results").getChildren();
-        for (Element result : results) {
-            String name = result.getChildText("name");
-            String layer = result.getChildText("layerName");
-            if (layer.equalsIgnoreCase("state")) {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode rootNode = mapper.readValue(inStream, JsonNode.class);
+        for (JsonNode node : rootNode) {
+            String field = node.get("field").getTextValue();
+            String value = node.get("value").getTextValue();
+
+            if (field.equalsIgnoreCase(COASTAL_WATERS_LAYER)) {
+                String state = value.replace(" (including Coastal Waters)", "");
+                state = state.replace("Captial", "Capital");
+                state = state.replace("Jervis Bay Territory", "Australian Capital Territory");
                 SensitivityZone zone;
-                if ((zone = SensitivityZoneFactory.getZoneByName(name)) != null) {
+                if ((zone = SensitivityZoneFactory.getZoneByName(state)) != null) {
                     zones.add(zone);
                 }
 
                 // TODO PFF PQA work around - remove when implemented in Gazetteer
-                if (name.equalsIgnoreCase("Queensland") &&
-                    layer.equalsIgnoreCase("state") &&
+                if (state.equalsIgnoreCase("Queensland") &&
                     NumberUtils.toFloat(latitude) >= -19.0 &&
                     NumberUtils.toFloat(longitude) >= 144.25) {
                     zones.add(SensitivityZoneFactory.getZone(SensitivityZone.PFFPQA1995));
                 }
-            }
 
-            if (layer.equalsIgnoreCase("cw")) {
-                SensitivityZone zone;
-                String state = name.replace(" (including Coastal Waters)", "");
-                if ((zone = SensitivityZoneFactory.getZoneByName(state)) != null) {
-                    zones.add(zone);
-                }
-            }
-
-            if (layer.equalsIgnoreCase("lga")) {
+            } else if (field.equalsIgnoreCase(LGA_BOUNDARIES_LAYER)) {
                 // TODO Special zones work around - remove when implemented in Gazetteer
-                if (name.equalsIgnoreCase("Bauhinia (Queensland)") ||
-                    name.equalsIgnoreCase("Emerald (Queensland)") ||
-                    name.equalsIgnoreCase("Peak Downs (Queensland)")) {
+                if (value.equalsIgnoreCase("Bauhinia") ||
+                    value.equalsIgnoreCase("Emerald") ||
+                    value.equalsIgnoreCase("Peak Downs")) {
                     // Emerald Citrus Canker PQA
                     zones.add(SensitivityZoneFactory.getZone(SensitivityZone.ECCPQA2004));
-                } else if (name.equalsIgnoreCase("Wacol (Queensland)")) {
+                } else if (value.equalsIgnoreCase("Wacol")) {
                     // Red Imported Fire Ant
                     zones.add(SensitivityZoneFactory.getZone(SensitivityZone.RIFARA));
-                } else if (name.equalsIgnoreCase("Kubin (Queensland)") || name.equalsIgnoreCase("Badu (Queensland)")) {
+                } else if (value.equalsIgnoreCase("Kubin") || value.equalsIgnoreCase("Badu")) {
                     // Torres Strait Protected Zone
                     zones.add(SensitivityZoneFactory.getZone(SensitivityZone.TSPZ));
-                } else if (name.equalsIgnoreCase("Hammond (Queensland)") || name.equalsIgnoreCase("Torres (Queensland)")) {
+                } else if (value.equalsIgnoreCase("Hammond") || value.equalsIgnoreCase("Torres")) {
                     // Special Quarantine Zone
                     zones.add(SensitivityZoneFactory.getZone(SensitivityZone.TSSQZ));
-                } else if (name.equalsIgnoreCase("Albury (New South Wales)")) {
+                } else if (value.equalsIgnoreCase("Albury")) {
                     // Phylloxera Infested Zone
                     zones.add(SensitivityZoneFactory.getZone(SensitivityZone.PIZNSWAC));
                 }
