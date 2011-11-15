bbd061b496ec72d706207f675249c3f8c60adf6b
peter.flemming@csiro.au <peter.flemming@csiro.au>
2011-11-15 00:41:22 +0000

    Additional fields and methods.
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/sensitive-species@2879 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/java/au/org/ala/sds/model/SensitivityInstance.java
+++ src/main/java/au/org/ala/sds/model/SensitivityInstance.java
@@ -1,6 +1,8 @@
     package au.org.ala.sds.model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
 *
@@ -10,9 +12,13 @@ public class SensitivityInstance implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    public static final String BIRDS_AUSTRALIA_INSTANCE = "Birds Australia";
+
     private final SensitivityCategory category;
     private final String authority;
     private final SensitivityZone zone;
+    private final String reason = null;
+    private final String remarks = null;
 
     public SensitivityInstance(SensitivityCategory category, String authority, SensitivityZone zone) {
         this.category = category;
@@ -30,6 +36,38 @@ public class SensitivityInstance implements Serializable {
 
     public SensitivityZone getZone() {
         return zone;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public static void removeInstance(List<SensitivityInstance> instances, String authority) {
+        List<SensitivityInstance> authInstances = new ArrayList<SensitivityInstance>();
+        for (SensitivityInstance instance : instances) {
+            if (instance.authority.equalsIgnoreCase(authority)) {
+                authInstances.add(instance);
+            }
+        }
+        for (SensitivityInstance authInstance : authInstances) {
+            instances.remove(authInstance);
+        }
+    }
+
+    public static void removeAllButInstance(List<SensitivityInstance> instances, String authority) {
+        List<SensitivityInstance> notAuthInstances = new ArrayList<SensitivityInstance>();
+        for (SensitivityInstance instance : instances) {
+            if (!instance.authority.equalsIgnoreCase(authority)) {
+                notAuthInstances.add(instance);
+            }
+        }
+        for (SensitivityInstance authInstance : notAuthInstances) {
+            instances.remove(authInstance);
+        }
     }
 
 }
