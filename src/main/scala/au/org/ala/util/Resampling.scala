f6161fbde4e6155a14e4eaf5d1b752a3848a68e3
djtfmartin <djtfmartin@gmail.com>
2011-12-12 11:25:24 +0000

    refactored resampling
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/biocache-store@2948 1ef1b718-b4f5-11dd-90a5-ed332e521da9

---
+++ src/main/scala/au/org/ala/util/Resampling.scala
@@ -0,0 +1,93 @@
+package au.org.ala.util
+
+import au.org.ala.biocache.Config
+import au.com.bytecode.opencsv.{CSVWriter,CSVReader}
+import scala.collection.mutable.HashSet
+import java.io.{FileReader, FileWriter}
+
+/**
+ * A re-sampler for sensitive records.
+ */
+object ResampleSensitiveRecords extends ResampleRecords {
+
+  def sensitiveFilter(map:Map[String, String]) : Boolean = (map.getOrElse("originalSensitiveValues","") != "")
+
+  def main(args:Array[String]){
+    val r = new ResampleRecords
+    r.resamplePointsByFilter(sensitiveFilter, Array("originalSensitiveValues"))
+  }
+}
+
+/**
+ * Class that supports re-sampling of records based on a supplied filter. This class does the following:
+ *
+ * 1) Retrieves a distinct list of points based on a filter
+ * 2) Retrieve a list of record based on a filter
+ * 3) Performs sampling for the list of points
+ * 4) Loads the results of the sampling
+ * 5) Reprocesses the records in the supplied list
+ *
+ * Note: if the filter relies on certain properties, these properties must be listed in the
+ * fieldsRequired.
+ */
+class ResampleRecords {
+
+  val recordsSampledFilePath = "/tmp/records-resampled.txt"
+  val pointsSampledFilePath = "/tmp/points-resampled.txt"
+  val pointsResampledFilePath = "/tmp/points-resampled-sampled.txt"
+
+  /**
+   * Resample and reprocess records matching the filter
+   */
+  def resamplePointsByFilter(filter:Map[String, String] => Boolean, fieldsRequired:Array[String]){
+
+    println("Starting the re-sampling.....")
+
+    val records = new CSVWriter(new FileWriter(recordsSampledFilePath))
+    val distinctPoints = new HashSet[(String, String)]
+
+    val fields = Array("decimalLatitude.p", "decimalLongitude.p") ++ fieldsRequired
+
+    //iterate through records
+    //produce two CSV files: 1) Rowkeys and  2) distinct decimalLatitude.p, decimalLongitude.p
+    Config.persistenceManager.pageOverSelect("occ", (guid,map) => {
+      if (filter(map)){
+        distinctPoints += ((map.getOrElse("decimalLongitude.p",""), map.getOrElse("decimalLatitude.p","")))
+        records.writeNext(Array(guid))
+      }
+      true
+    }, "","", 1000, fields:_*)
+    records.flush
+    records.close
+
+    //produce a distinct list of coordinates from first CSV
+    val distinctPointsFile = new CSVWriter(new FileWriter(pointsSampledFilePath))
+    distinctPoints.foreach(c => distinctPointsFile.writeNext(Array(c._1,c._2)))
+    distinctPointsFile.flush
+    distinctPointsFile.close
+
+    //sample with the supplied points
+    val sampling = new Sampling
+    sampling.sampling(pointsSampledFilePath, pointsResampledFilePath)
+    sampling.loadSampling(pointsResampledFilePath)
+
+    //reprocess the records listed in first CSV
+    val pointsReader = new CSVReader(new FileReader(recordsSampledFilePath))
+    val rp = new RecordProcessor
+    var current = pointsReader.readNext
+    while (current != null){
+      if(current.length == 1){
+        Config.occurrenceDAO.getRawProcessedByRowKey(current(0)) match {
+          case Some(rawProcessed) => {
+            rp.processRecord(rawProcessed(0), rawProcessed(1))
+          }
+          case None => {}
+        }
+      }
+      current = pointsReader.readNext
+    }
+    pointsReader.close
+    Config.persistenceManager.shutdown //close DB connections
+    println("Finished the re-sampling.")
+  }
+}
