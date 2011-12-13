0498b8234dec8f3b3375065a5ec6647b585a7099
djtfmartin <djtfmartin@gmail.com>
2011-12-13 06:39:36 +0000

    DB migration util
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/biocache-store@2954 1ef1b718-b4f5-11dd-90a5-ed332e521da9

---
+++ src/main/scala/au/org/ala/util/MigrationUtil.scala
@@ -0,0 +1,60 @@
+package au.org.ala.util
+
+import au.org.ala.biocache.CassandraPersistenceManager
+
+object MigrationUtil {
+
+  val elPattern = """el[0-9]{1,}\.p""".r
+  val clPattern = """cl[0-9]{1,}\.p""".r
+
+  def isEL(str:String) = !elPattern.findFirstMatchIn(str).isEmpty
+  def isCL(str:String) = !clPattern.findFirstMatchIn(str).isEmpty
+
+  def main(args:Array[String]){
+
+    var sourceHost = ""
+    var sourcePort = -1
+    var targetHost = ""
+    var targetPort = -1
+    var keyspace = ""
+    val obsCols = List("originalDecimalLatitude","originalDecimalLongitude","originalLocality",
+      "originalLocationRemarks","originalVerbatimLatitude","originalVerbatimLongitude","lastLoadTime")
+
+    val parser = new OptionParser("Cassandra migration") {
+        arg("source-host", "", {v:String => sourceHost = v})
+        arg("source-port", "", {v:String => sourcePort = v.toInt})
+        arg("target-host", "", {v:String => targetHost = v})
+        arg("target-port", "", {v:String => targetPort = v.toInt})
+        arg("keyspace", "", {v:String => keyspace = v})
+    }
+
+    var counter = 0
+    var start = System.currentTimeMillis()
+    var finish = System.currentTimeMillis()
+    if(parser.parse(args)){
+      //page through source keyspace, and add record to target
+      val sourcePM = new CassandraPersistenceManager(sourceHost, sourcePort, "source", keyspace)
+      val targetPM = new CassandraPersistenceManager(targetHost, targetPort, "target", keyspace)
+
+      sourcePM.pageOverAll(keyspace, (guid, map) => {
+        counter += 1
+        if (counter % 1000 == 0){
+          finish = System.currentTimeMillis()
+          println(">> processing record: " + counter +", time taken for last 1000: " + ((finish.toFloat-start.toFloat) / 1000))
+          start = System.currentTimeMillis()
+        }
+        val deleted = map.getOrElse("deleted", "false").toBoolean
+        if (!deleted){
+          val mapFiltered = map.filter({ case (key, value) => {
+            if(isEL(key)) false
+            else if (isCL(key)) false
+            else if (obsCols.contains(key)) false
+            else true
+          }})
+          targetPM.put(guid, keyspace, mapFiltered)
+        }
+        true
+      })
+    }
+  }
+}
