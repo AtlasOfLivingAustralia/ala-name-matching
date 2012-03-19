90904223cf81c0ca1e74840f7f3ad35834c5f0e0
natasha.carter@csiro.au <natasha.carter@csiro.au>
2012-03-19 03:27:12 +0000

    Index changes for some AVH fields.
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/biocache-store@3081 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/scala/au/org/ala/biocache/indexdao.scala
+++ src/main/scala/au/org/ala/biocache/indexdao.scala
@@ -128,7 +128,7 @@ trait IndexDAO {
       "occurrence_remarks", "citation", "user_assertions", "system_assertions", "collector","state_conservation","raw_state_conservation",
       "sensitive", "coordinate_uncertainty", "user_id","provenance","subspecies_guid", "subspecies_name", "interaction","last_assertion_date",
       "last_load_date","last_processed_date", "modified_date", "establishment_means","loan_number","loan_identifier","loan_destination",
-      "loan_botanist","loan_date", "loan_return_date","original_name_usage","duplicate_inst", "record_number","first_loaded_date","name_match_metric") //++ elFields ++ clFields
+      "loan_botanist","loan_date", "loan_return_date","original_name_usage","duplicate_inst", "record_number","first_loaded_date","name_match_metric","life_stage") //++ elFields ++ clFields
 
   /**
    * Constructs a scientific name.
@@ -371,10 +371,11 @@ trait IndexDAO {
                     if(map.contains("loanDate")) map.getOrElse("loanDate","") + "T00:00:00Z" else "",
                     if(map.contains("loanReturnDate")) map.getOrElse("loanReturnDate","") + "T00:00:00Z" else "",
                     map.getOrElse("originalNameUsage", map.getOrElse("typifiedName","")),
-                    map.getOrElse("duplicates","").replaceAll(",","|"),
+                    map.getOrElse("duplicates",""),//.replaceAll(",","|"),
                     map.getOrElse("recordNumber",""),
                     if(firstLoadDate.isEmpty)"" else DateFormatUtils.format(firstLoadDate.get, "yyyy-MM-dd'T'HH:mm:ss'Z'"),
-                    map.getOrElse("nameMatchMetric.p", "")
+                    map.getOrElse("nameMatchMetric.p", ""), 
+                    map.getOrElse("phenology","")  //TODO make this a controlled vocab that gets mapped during processing...
                     ) //++ elFields.map(field => getValue(field, map)) ++ clFields.map(field=> getValue(field, map))
             }
             else {
