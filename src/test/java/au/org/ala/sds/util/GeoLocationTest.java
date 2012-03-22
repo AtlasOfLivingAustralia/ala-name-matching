176df85bb6f23a0bbd91cdd49943e77faf3970bc
peter.flemming@csiro.au <peter.flemming@csiro.au>
2012-03-22 05:50:29 +0000

    Added additional Plant Pest layer support.
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/sensitive-species@3094 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/test/java/au/org/ala/sds/util/GeoLocationTest.java
+++ src/test/java/au/org/ala/sds/util/GeoLocationTest.java
@@ -60,5 +60,88 @@ public class GeoLocationTest {
         zones = GeoLocationHelper.getZonesContainingPoint("-23.5774", "148.885775");
         assertFalse(zones.contains(SensitivityZoneFactory.getZone(SensitivityZone.ECCPQA2004)));
 
+        // Torres Strait Protection Zone
+        zones = GeoLocationHelper.getZonesContainingPoint("-9.80699", "142.64282");
+        assertTrue(zones.contains(SensitivityZoneFactory.getZone(SensitivityZone.TSPZ)));
+
+        // Torres Strait Special Quarantine Zone
+        zones = GeoLocationHelper.getZonesContainingPoint("-10.62870", "142.19788");
+        assertTrue(zones.contains(SensitivityZoneFactory.getZone(SensitivityZone.TSSQZ)));
+
+        // Fruit Fly Exclusion Zone - Tri State NSW
+        zones = GeoLocationHelper.getZonesContainingPoint("-33.53336", "142.08734");
+        assertTrue(zones.contains(SensitivityZoneFactory.getZone(SensitivityZone.FFEZ)));
+        assertTrue(zones.contains(SensitivityZoneFactory.getZone(SensitivityZone.NSW)));
+
+        // Fruit Fly Exclusion Zone - Tri State SA
+        zones = GeoLocationHelper.getZonesContainingPoint("-34.38989", "140.52728");
+        assertTrue(zones.contains(SensitivityZoneFactory.getZone(SensitivityZone.FFEZ)));
+        assertTrue(zones.contains(SensitivityZoneFactory.getZone(SensitivityZone.SA)));
+
+        // Fruit Fly Exclusion Zone - Tri State Vic
+        zones = GeoLocationHelper.getZonesContainingPoint("-36.17444", "144.29559");
+        assertTrue(zones.contains(SensitivityZoneFactory.getZone(SensitivityZone.FFEZ)));
+        assertTrue(zones.contains(SensitivityZoneFactory.getZone(SensitivityZone.VIC)));
+
+        // Potato Cyst Nematode - Wandin
+        zones = GeoLocationHelper.getZonesContainingPoint("-37.82010", "145.45541");
+        assertTrue(zones.contains(SensitivityZoneFactory.getZone(SensitivityZone.PCNCAVICWAN)));
+        assertTrue(zones.contains(SensitivityZoneFactory.getZone(SensitivityZone.VIC)));
+
+        // Potato Cyst Nematode - Gembrook
+        zones = GeoLocationHelper.getZonesContainingPoint("-37.93933", "145.53094");
+        assertTrue(zones.contains(SensitivityZoneFactory.getZone(SensitivityZone.PCNCAVICGEM)));
+        assertTrue(zones.contains(SensitivityZoneFactory.getZone(SensitivityZone.VIC)));
+
+        // Potato Cyst Nematode - Koo Wee Rup
+        zones = GeoLocationHelper.getZonesContainingPoint("-38.13727", "145.59823");
+        assertTrue(zones.contains(SensitivityZoneFactory.getZone(SensitivityZone.PCNCAVICKWR)));
+        assertTrue(zones.contains(SensitivityZoneFactory.getZone(SensitivityZone.VIC)));
+
+        // Potato Cyst Nematode - Thorpedale
+        zones = GeoLocationHelper.getZonesContainingPoint("-38.35190", "146.14480");
+        assertTrue(zones.contains(SensitivityZoneFactory.getZone(SensitivityZone.PCNCAVICTHO)));
+        assertTrue(zones.contains(SensitivityZoneFactory.getZone(SensitivityZone.VIC)));
+
+        // Phylloxera Infestation Zone Albury - Corowa
+        zones = GeoLocationHelper.getZonesContainingPoint("-35.99533", "146.88358");
+        assertTrue(zones.contains(SensitivityZoneFactory.getZone(SensitivityZone.PIZNSWAC)));
+        assertTrue(zones.contains(SensitivityZoneFactory.getZone(SensitivityZone.NSW)));
+
+        // Phylloxera Infestation Zone Sydney
+        zones = GeoLocationHelper.getZonesContainingPoint("-34.13686", "150.99214");
+        assertTrue(zones.contains(SensitivityZoneFactory.getZone(SensitivityZone.PIZNSWSR)));
+        assertTrue(zones.contains(SensitivityZoneFactory.getZone(SensitivityZone.NSW)));
+
+        // Phylloxera Infestation Zone Vic North East
+        zones = GeoLocationHelper.getZonesContainingPoint("-36.35326", "146.68664");
+        assertTrue(zones.contains(SensitivityZoneFactory.getZone(SensitivityZone.PIZVICNE)));
+        assertTrue(zones.contains(SensitivityZoneFactory.getZone(SensitivityZone.VIC)));
+
+        // Phylloxera Infestation Zone Vic North East
+        zones = GeoLocationHelper.getZonesContainingPoint("-36.35326", "146.68664");
+        assertTrue(zones.contains(SensitivityZoneFactory.getZone(SensitivityZone.PIZVICNE)));
+        assertTrue(zones.contains(SensitivityZoneFactory.getZone(SensitivityZone.VIC)));
+
+        // Phylloxera Infestation Zone Vic Mooroopna
+        zones = GeoLocationHelper.getZonesContainingPoint("-36.41738", "145.29687");
+        assertTrue(zones.contains(SensitivityZoneFactory.getZone(SensitivityZone.PIZVICMOR)));
+        assertTrue(zones.contains(SensitivityZoneFactory.getZone(SensitivityZone.VIC)));
+
+        // Phylloxera Infestation Zone Vic Nagambie
+        zones = GeoLocationHelper.getZonesContainingPoint("-36.79892", "145.14681");
+        assertTrue(zones.contains(SensitivityZoneFactory.getZone(SensitivityZone.PIZVICNAG)));
+        assertTrue(zones.contains(SensitivityZoneFactory.getZone(SensitivityZone.VIC)));
+
+        // Phylloxera Infestation Zone Vic Maroondah
+        zones = GeoLocationHelper.getZonesContainingPoint("-37.65750", "145.37350");
+        assertTrue(zones.contains(SensitivityZoneFactory.getZone(SensitivityZone.PIZVICMAR)));
+        assertTrue(zones.contains(SensitivityZoneFactory.getZone(SensitivityZone.VIC)));
+
+        // Phylloxera Infestation Zone Vic Upton
+        zones = GeoLocationHelper.getZonesContainingPoint("-36.91657", "145.36814");
+        assertTrue(zones.contains(SensitivityZoneFactory.getZone(SensitivityZone.PIZVICUPT)));
+        assertTrue(zones.contains(SensitivityZoneFactory.getZone(SensitivityZone.VIC)));
+
     }
 }
