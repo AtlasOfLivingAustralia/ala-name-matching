43dcb75d90340de6e123060984089834cd2c371e
djtfmartin <djtfmartin@gmail.com>
2012-03-28 03:06:18 +0000

    script for running jackKnife
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/biocache-store@3115 1ef1b718-b4f5-11dd-90a5-ed332e521da9

---
+++ scripts/jackKnife.sh
@@ -0,0 +1,16 @@
+CP="-Dactors.corePoolSize=2 -Dactors.maxPoolSize=4 -Dactors.minPoolSize=2 -cp .:biocache.jar"
+EXPORTS="/data2/jackKnife/exports"
+SOLR=/data/solr/bio-proto/data/index
+
+echo "Exporting list of species & subspecies records...."
+java -cp $CP au.org.ala.util.ExportForOutliers $SOLR $EXPORTS
+sort -T $EXPORTS $EXPORTS/species-unsorted.txt > $EXPORTS/species-sorted.txt
+sort -T $EXPORTS $EXPORTS/subspecies-unsorted.txt > $EXPORTS/subspecies-sorted.txt
+
+echo "Start species outlier tests"
+java -Xmx4g -Xms4g $CP au.org.ala.biocache.outliers.SpeciesOutlierTests -fd $EXPORTS/species-sorted.txt -hfd "taxonConceptID uuid decimalLatitude decimalLongitude el882 el889 el887 el865 el894" -if $EXPORTS/species-idsToReindex.txt
+
+echo "Start subspecies outlier tests"
+java -Xmx4g -Xms4g $CP au.org.ala.biocache.outliers.SpeciesOutlierTests -fd $EXPORTS/subspecies-sorted.txt -hfd "taxonConceptID uuid decimalLatitude decimalLongitude el882 el889 el887 el865 el894" -if $EXPORTS/subspecies-idsToReindex.txt
+
+echo "Finished"
