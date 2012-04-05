15db9efb32914b6e70f3b8ad7d2525cba6da2e7a
peter.flemming@csiro.au <peter.flemming@csiro.au>
2012-04-05 05:26:19 +0000

    Updated tests
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/sensitive-species@3131 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/test/java/au/org/ala/sds/util/GeneralisedLocationTest.java
+++ src/test/java/au/org/ala/sds/util/GeneralisedLocationTest.java
@@ -49,10 +49,10 @@ public class GeneralisedLocationTest {
         ENDANGERED = SensitivityCategoryFactory.getCategory(SensitivityCategory.ENDANGERED);
         VULNERABLE = SensitivityCategoryFactory.getCategory(SensitivityCategory.VULNERABLE);
         NEAR_THREATENED = SensitivityCategoryFactory.getCategory(SensitivityCategory.NEAR_THREATENED);
-        instances.add(new ConservationInstance(CRITICALLY_ENDANGERED, "NSW DECCW", NSW, "WITHHOLD"));
-        instances.add(new ConservationInstance(ENDANGERED, "Vic DSE", VIC, "10km"));
-        instances.add(new ConservationInstance(VULNERABLE, "QLD DERM", QLD, "1km"));
-        instances.add(new ConservationInstance(NEAR_THREATENED, "Tas DPIPWE", TAS, "100m"));
+        instances.add(new ConservationInstance(CRITICALLY_ENDANGERED, "NSW DECCW", NSW, null, null, "WITHHOLD"));
+        instances.add(new ConservationInstance(ENDANGERED, "Vic DSE", VIC, null, null, "10km"));
+        instances.add(new ConservationInstance(VULNERABLE, "QLD DERM", QLD, null, null, "1km"));
+        instances.add(new ConservationInstance(NEAR_THREATENED, "Tas DPIPWE", TAS, null, null, "100m"));
     }
 
     @Test
