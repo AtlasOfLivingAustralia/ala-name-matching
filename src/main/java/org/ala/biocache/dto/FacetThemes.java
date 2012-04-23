c82d09f37f9a6af2abdb9fa0c529878f4b1cdde8
djtfmartin <djtfmartin@gmail.com>
2012-04-23 23:23:05 +0000

    additional facet, removal of old LGA facet
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/biocache-service@3195 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/java/org/ala/biocache/dto/FacetThemes.java
+++ src/main/java/org/ala/biocache/dto/FacetThemes.java
@@ -7,7 +7,7 @@ public class FacetThemes {
     public static java.util.List<FacetTheme> allThemes = new java.util.ArrayList<FacetTheme>();
     static {
         allThemes.add(new FacetTheme("Taxonomic", "taxon_name", "raw_taxon_name", "subspecies_name", "species_guid", "genus_guid","family","order","class", "phylum", "kingdom","species_group","rank","interaction","species_habitats"));
-        allThemes.add(new FacetTheme("Geospatial","uncertainty","sensitive","state_conservation","raw_state_conservation","places","state","country","biogeographic_region","ibra","imcra","cl918","cl617","cl620","geospatial_kosher"));
+        allThemes.add(new FacetTheme("Geospatial","uncertainty","sensitive","state_conservation","raw_state_conservation","cl959","state","country","biogeographic_region","ibra","imcra","cl966","cl918","cl617","cl620","geospatial_kosher"));
         allThemes.add(new FacetTheme("Temporal","month","year","decade"));
         allThemes.add(new FacetTheme("Record Details", "basis_of_record","type_status","multimedia","collector"));
         allThemes.add(new FacetTheme("Attribution", "data_provider_uid","data_resource_uid","institution_uid","collection_uid","provenance"));
