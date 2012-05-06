5cba46230d59d0c8e3be57667568535cbe53d116
natasha.carter@csiro.au <natasha.carter@csiro.au>
2012-05-06 23:42:20 +0000

    Enhancement to allow a start and end key to be optionally provided.
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/biocache-store@3222 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/scala/au/org/ala/util/DeleteColumn.scala
+++ src/main/scala/au/org/ala/util/DeleteColumn.scala
@@ -8,27 +8,41 @@ import au.org.ala.biocache._
 object DeleteColumn {
 
   def main(args: Array[String]): Unit = {
-    if(args.length<2){
-        println("Usage: entityName column1 column2 column3 column4")
-        exit(1)
+//    if(args.length<2){
+//        println("Usage: entityName column1 column2 column3 column4")
+//        exit(1)
+//    }
+    var startUuid =""
+    var endUuid ="" 
+    var entityName=""
+    var columnsToDelete=Array[String]()
+    
+    val parser = new OptionParser("delete column options") {
+        arg("<entity>", "the entity (column family in cassandra) to export from", { v: String => entityName = v })
+        arg("<cols to delete>","A CSV list of columns to be deleted",{v:String => columnsToDelete = v.split(",")})
+        opt("s", "start","The record to start with", {v:String => startUuid = v})
+        opt("e", "end", "The record to end with", {v:String =>endUuid=v})
     }
-    
-    val entityName = args(0)
-    val columnsToDelete = args.tail
-    printf("Deleting from entity %s, columns: %s. Hit return to proceed...", entityName, entityName.mkString(", "))
-    val line = readLine
-    println("proceeding.")
-    
-    Config.persistenceManager.pageOverSelect(entityName, (guid, map)=> {
-        map.keys.foreach( key => {
-            if(!map.get(key).isEmpty){
-            	Config.persistenceManager.deleteColumns(guid, entityName, key)
-            }
-        })
-        true
-    }, "","", 1000, args.tail:_*)
-    
-    Config.persistenceManager.shutdown
-    println("Complete.")
+    if(parser.parse(args)){
+        //val entityName = args(0)
+       // val columnsToDelete = args.tail
+        printf("Deleting from entity %s, columns: %s. Starting with row %s, ending with %s Hit return to proceed...", 
+            entityName, columnsToDelete.mkString(", "), startUuid, endUuid)
+        val line = readLine
+        println("proceeding.")
+        
+        Config.persistenceManager.pageOverSelect(entityName, (guid, map)=> {
+            map.keys.foreach( key => {
+                if(!map.get(key).isEmpty){
+                	Config.persistenceManager.deleteColumns(guid, entityName, key)
+                }
+            })
+            true
+        }, startUuid,endUuid, 1000, columnsToDelete:_*)
+        
+        
+        Config.persistenceManager.shutdown
+        println("Complete.")
+    }
   }
 }
