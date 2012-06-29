3b0937f124e4947efbc124b600790b9430e8df4d
natasha.carter@csiro.au <natasha.carter@csiro.au>
2012-06-29 01:52:10 +0000

    Added sensitive coordinate uncertainty.
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/biocache-service@3337 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/java/org/ala/biocache/dto/SensitiveOccurrenceIndex.java
+++ src/main/java/org/ala/biocache/dto/SensitiveOccurrenceIndex.java
@@ -5,6 +5,7 @@ import org.apache.solr.client.solrj.beans.Field;
 public class SensitiveOccurrenceIndex extends OccurrenceIndex {
     @Field("sensitive_latitude")  Double sensitiveDecimalLatitude;
     @Field("sensitive_longitude")  Double sensitiveDecimalLongitude;
+    @Field("sensitive_coordinate_uncertainty") Double sensitiveCoordinateUncertaintyInMeters;
     /**
      * @return the sensitiveDecimalLatitude
      */
@@ -28,6 +29,19 @@ public class SensitiveOccurrenceIndex extends OccurrenceIndex {
      */
     public void setSensitiveDecimalLongitude( Double sensitiveDecimalLongitude) {
         this.sensitiveDecimalLongitude = sensitiveDecimalLongitude;
+    }
+    /**
+     * @return the sensitiveCoordinateUncertaintyInMeters
+     */
+    public Double getSensitiveCoordinateUncertaintyInMeters() {
+      return sensitiveCoordinateUncertaintyInMeters;
+    }
+    /**
+     * @param sensitiveCoordinateUncertaintyInMeters the sensitiveCoordinateUncertaintyInMeters to set
+     */
+    public void setSensitiveCoordinateUncertaintyInMeters(
+        Double sensitiveCoordinateUncertaintyInMeters) {
+      this.sensitiveCoordinateUncertaintyInMeters = sensitiveCoordinateUncertaintyInMeters;
     }
     
     
