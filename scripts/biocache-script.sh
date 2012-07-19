91c1da22a998a3868adcee246238369771008549
djtfmartin <djtfmartin@gmail.com>
2012-07-19 00:18:22 +0000

    scriptool
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/biocache-store@3362 1ef1b718-b4f5-11dd-90a5-ed332e521da9

---
+++ scripts/biocache-script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Make sure only tomcat can run our script
+if [ $(whoami) != "tomcat" ]
+then
+   echo "This script must be run as the tomcat user."
+   exit 1
+fi
+
+BIOCACHE_INSTALL=/usr/local/biocache
+CLASSPATH=$BIOCACHE_INSTALL:$BIOCACHE_INSTALL/biocache.jar
+java -Xmx16g -Xmx16g -cp $CLASSPATH -Dactors.corePoolSize=8 -Dactors.maxPoolSize=16 -Dactors.minPoolSize=8 au.org.ala.util.ScriptTool
