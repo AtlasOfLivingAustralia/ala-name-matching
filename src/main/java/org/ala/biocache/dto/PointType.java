86bbd5bd8787b889b3e34dfe96277d086e8174df
djtfmartin <djtfmartin@gmail.com>
2012-09-14 02:42:33 +0000

    getfeatureinfo debug
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/biocache-service@3447 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/java/org/ala/biocache/dto/PointType.java
+++ src/main/java/org/ala/biocache/dto/PointType.java
@@ -1,5 +1,7 @@
 package org.ala.biocache.dto;
 
+import java.math.BigDecimal;
+
 /**
  * Enum to store the groupings or lat/long accuracy level (for zoom levels)
  *
@@ -9,18 +11,18 @@ package org.ala.biocache.dto;
  * @author "Nick dos Remedios <Nick.dosRemedios@csiro.au>"
  */
 public enum PointType {
-    POINT_1     ("point-1", 1.0f, 1f),
-    POINT_01    ("point-0.1", 0.1f, 10f),
-    POINT_001   ("point-0.01", 0.01f, 100f),
-    POINT_0001  ("point-0.001", 0.001f, 1000f),
-    POINT_00001 ("point-0.0001", 0.0001f, 10000f),
-    POINT_RAW   ("lat_long", 0f, 10000f); //negative value to indicate not to round
+    POINT_1     ("point-1", 1.0f, 0),
+    POINT_01    ("point-0.1", 0.1f, 1),
+    POINT_001   ("point-0.01", 0.01f, 2),
+    POINT_0001  ("point-0.001", 0.001f, 3),
+    POINT_00001 ("point-0.0001", 0.0001f, 4),
+    POINT_RAW   ("lat_long", 0f, 5); //negative value to indicate not to round
     
     private String label;
     private Float value;
-    private Float rounding;
+    private int rounding;
 
-    PointType(String label,Float value, Float rounding) {
+    PointType(String label,Float value, int rounding) {
         this.label = label;
         this.value = value;
         this.rounding = rounding;
@@ -42,10 +44,44 @@ public enum PointType {
         this.value = value;
     }
 
-    public double roundToPointType(double value){
-        //if(rounding>0)
-            return Math.floor(value * rounding) / rounding;
-//        else
-//            return value;
+//    public double roundToPointType(double unrounded){
+//
+////    BigDecimal bd = new BigDecimal(unrounded);
+////    BigDecimal rounded = bd.setScale(precision, BigDecimal.ROUND_DOWN);
+////    return rounded.doubleValue();
+//
+//       return Math.floor(value * rounding) / rounding;
+//    }
+
+    public double roundDownToPointType(double value){
+        BigDecimal bd = new BigDecimal(value);
+        BigDecimal rounded = bd.setScale(rounding, BigDecimal.ROUND_FLOOR);
+        return rounded.floatValue();
     }
+
+    public double roundUpToPointType(double value){
+        BigDecimal bd = new BigDecimal(value);
+        BigDecimal rounded = bd.setScale(rounding, BigDecimal.ROUND_CEILING);
+        return rounded.floatValue();
+    }
+
+    public double roundToPointType(double value){
+        BigDecimal bd = new BigDecimal(value);
+        BigDecimal rounded = bd.setScale(rounding, BigDecimal.ROUND_HALF_UP);
+        return rounded.floatValue();
+    }
+
+
+//
+//    public static void main(String[] args){
+//        BigDecimal bd = new BigDecimal(-8.12321312);
+//        BigDecimal rounded = bd.setScale(2, BigDecimal.ROUND_CEILING);
+//        System.out.println(rounded.floatValue());
+//
+//        BigDecimal bd2 = new BigDecimal(-10.12321312);
+//        BigDecimal rounded2 = bd2.setScale(2, BigDecimal.ROUND_CEILING);
+//        System.out.println(rounded2.floatValue());
+//
+//    }
+
 }
