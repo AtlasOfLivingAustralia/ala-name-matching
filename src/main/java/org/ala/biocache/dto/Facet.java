b7d228e03f46aef9d4466c47536f09928e9f58c4
djtfmartin <djtfmartin@gmail.com>
2012-10-02 10:32:38 +0000

    support for dynamic facets
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/biocache-service@3473 1ef1b718-b4f5-11dd-90a5-ed332e521da9

---
+++ src/main/java/org/ala/biocache/dto/Facet.java
@@ -0,0 +1,30 @@
+package org.ala.biocache.dto;
+
+public class Facet {
+
+    protected String name;
+    protected String displayName;
+
+    public Facet() {}
+
+    public Facet(String name, String displayName) {
+        this.name = name;
+        this.displayName = displayName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+}
