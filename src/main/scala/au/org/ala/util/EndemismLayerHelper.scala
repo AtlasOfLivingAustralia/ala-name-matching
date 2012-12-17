417ccbd4b1590c5fe3a839c2e1cd59dd2574b115
chris.flemming.ala@gmail.com <chris.flemming.ala@gmail.com>
2012-12-17 07:15:15 +0000

    Update endemism layer helper to generate data for 1 degree, 0.1 degree and 0.01 degree grid sizes.
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/biocache-store@3642 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/scala/au/org/ala/util/EndemismLayerHelper.scala
+++ src/main/scala/au/org/ala/util/EndemismLayerHelper.scala
@@ -31,25 +31,29 @@ object EndemismLayerHelper {
 
   def main(args: Array[String]) {
     val helper = new EndemismLayerHelper();
-    var allSpecies = false;
-    var speciesCellCountsFilePath: String = null;
-    var cellSpeciesFilePath: String = null;
+    //var allSpecies = false;
+    var outputFileDirectory: String = null;
+    var speciesCellCountsFilePrefix: String = null;
+    var cellSpeciesFilePrefix: String = null;
 
     val parser = new OptionParser("Find expert distribution outliers") {
-      arg("speciesCellCountsFilePath", "File to write species cell counts to", {
-        v: String => speciesCellCountsFilePath = v
+      arg("outputFileDirectory", "Directory in which to write the output files", {
+        v: String => outputFileDirectory = v
       })
-      arg("cellSpeciesFilePath", "File to write cell species lists to", {
-        v: String => cellSpeciesFilePath = v
+      arg("speciesCellCountsFilePrefix", "Prefix for files containing species cell counts (without .txt extension)", {
+        v: String => speciesCellCountsFilePrefix = v
       })
-      booleanOpt("a", "allSpecies", "If true, endemism values will be calcuated for all species, instead of those that were recently updated.", {
-        v: Boolean => allSpecies = v
+      arg("cellSpeciesFilePrefix", "Prefix for files containing cell species lists (without .txt extension)", {
+        v: String => cellSpeciesFilePrefix = v
       })
+      //      booleanOpt("a", "allSpecies", "If true, endemism values will be calcuated for all species, instead of those that were recently updated.", {
+      //        v: Boolean => allSpecies = v
+      //      })
     }
 
-    if (parser.parse(args)) {
-      helper.calculateSpeciesEndemismValues(speciesCellCountsFilePath, cellSpeciesFilePath, allSpecies)
-    }
+    //if (parser.parse(args)) {
+      helper.calculateSpeciesEndemismValues(outputFileDirectory, speciesCellCountsFilePrefix, cellSpeciesFilePrefix)
+    //}
   }
 }
 
@@ -57,43 +61,80 @@ class EndemismLayerHelper {
 
   val indexDAO = Config.indexDAO
 
-  def calculateSpeciesEndemismValues(speciesCellCountsFilePath: String, cellSpeciesFilePath: String, allSpecies: Boolean) {
-    var cellSpecies = Map[String, Set[String]]()
-    var speciesCellCounts = Map[String, Int]()
+  def calculateSpeciesEndemismValues(outputFileDirectory: String, speciesCellCountsFilePrefix: String, cellSpeciesFilePrefix: String) {
+    // Data for 0.01 degree by 0.01 degree cells
+    var cellSpeciesPoint01Degree = scala.collection.mutable.Map[String, Set[String]]()
+    var speciesCellCountsPoint01Degree = scala.collection.mutable.Map[String, Int]()
+
+    // Data for 0.1 degree by 0.1 degree cells
+    var cellSpeciesPoint1Degree = scala.collection.mutable.Map[String, Set[String]]()
+    var speciesCellCountsPoint1Degree = scala.collection.mutable.Map[String, Int]()
+
+    // Data for 1 degree by 1 degree cells
+    var cellSpecies1Degree = scala.collection.mutable.Map[String, Set[String]]()
+    var speciesCellCounts1Degree = scala.collection.mutable.Map[String, Int]()
 
     // get list of species
     val speciesLsids = doFacetQuery(EndemismLayerHelper.ALL_SPECIES_QUERY, EndemismLayerHelper.SPECIES_FACET)
 
     for (lsid <- speciesLsids) {
+      println(lsid)
       val occurrencePoints = doFacetQuery(MessageFormat.format(EndemismLayerHelper.SPECIES_QUERY_TEMPLATE, ClientUtils.escapeQueryChars(lsid)), EndemismLayerHelper.POINT_001_FACET)
 
-      var pointsSet = Set[String]()
+      // process for 0.01 degree resolution
+      processOccurrencePoints(occurrencePoints, lsid, cellSpeciesPoint01Degree, speciesCellCountsPoint01Degree, 2)
 
-      for (point <- occurrencePoints) {
-        val splitPoint = point.split(",")
-        val strLatitude = splitPoint(0)
-        val strLongitude = splitPoint(1)
+      // process for 0.1 degree resolution
+      processOccurrencePoints(occurrencePoints, lsid, cellSpeciesPoint1Degree, speciesCellCountsPoint1Degree, 1)
 
-        val roundedLatitude = Precision.round(java.lang.Double.parseDouble(strLatitude), 2, java.math.BigDecimal.ROUND_CEILING)
-        val roundedLongitude = Precision.round(java.lang.Double.parseDouble(strLongitude), 2, java.math.BigDecimal.ROUND_FLOOR)
-
-        val strRoundedCoords = roundedLatitude + "," + roundedLongitude
-        pointsSet += strRoundedCoords
-
-        var thisCellSpecies = cellSpecies.getOrElse(strRoundedCoords, null)
-        if (thisCellSpecies == null) {
-          thisCellSpecies = Set[String]()
-        }
-
-        thisCellSpecies += lsid
-        cellSpecies += (strRoundedCoords -> thisCellSpecies)
-      }
-
-      speciesCellCounts += (lsid -> pointsSet.size)
-      println(lsid + " - " + pointsSet.size + " cells")
+      // process for 0.1 degree resolution
+      processOccurrencePoints(occurrencePoints, lsid, cellSpecies1Degree, speciesCellCounts1Degree, 0)
     }
 
-    val bwSpeciesCellCounts = new BufferedWriter(new FileWriter(speciesCellCountsFilePath));
+    //write output for 0.01 degree resolution
+    val cellSpeciesFilePoint01Degree = outputFileDirectory + '/' + cellSpeciesFilePrefix + "-0.01-degree.txt"
+    val speciesCellCountsFilePoint01Degree = outputFileDirectory + '/' + speciesCellCountsFilePrefix + "-0.01-degree.txt"
+    writeFileOutput(cellSpeciesPoint01Degree, speciesCellCountsPoint01Degree, cellSpeciesFilePoint01Degree, speciesCellCountsFilePoint01Degree)
+
+    //write output for 0.1 degree resolution
+    val cellSpeciesFilePoint1Degree = outputFileDirectory + '/' + cellSpeciesFilePrefix + "-0.1-degree.txt"
+    val speciesCellCountsFilePoint1Degree = outputFileDirectory + '/' + speciesCellCountsFilePrefix + "-0.1-degree.txt"
+    writeFileOutput(cellSpeciesPoint1Degree, speciesCellCountsPoint1Degree, cellSpeciesFilePoint1Degree, speciesCellCountsFilePoint1Degree)
+
+    //write output for 1 degree resolution
+    val cellSpeciesFile1Degree = outputFileDirectory + '/' + cellSpeciesFilePrefix + "-1-degree.txt"
+    val speciesCellCountsFile1Degree = outputFileDirectory + '/' + speciesCellCountsFilePrefix + "-1-degree.txt"
+    writeFileOutput(cellSpecies1Degree, speciesCellCounts1Degree, cellSpeciesFile1Degree, speciesCellCountsFile1Degree)
+  }
+
+  def processOccurrencePoints(occurrencePoints: ListBuffer[String], lsid: String, cellSpecies: scala.collection.mutable.Map[String, Set[String]], speciesCellCounts: scala.collection.mutable.Map[String, Int], numDecimalPlacesToRoundTo: Int) {
+    var pointsSet = Set[String]()
+
+    for (point <- occurrencePoints) {
+      val splitPoint = point.split(",")
+      val strLatitude = splitPoint(0)
+      val strLongitude = splitPoint(1)
+
+      val roundedLatitude = Precision.round(java.lang.Double.parseDouble(strLatitude), numDecimalPlacesToRoundTo, java.math.BigDecimal.ROUND_CEILING)
+      val roundedLongitude = Precision.round(java.lang.Double.parseDouble(strLongitude), numDecimalPlacesToRoundTo, java.math.BigDecimal.ROUND_FLOOR)
+
+      val strRoundedCoords = roundedLatitude + "," + roundedLongitude
+      pointsSet += strRoundedCoords
+
+      var thisCellSpecies = cellSpecies.getOrElse(strRoundedCoords, null)
+      if (thisCellSpecies == null) {
+        thisCellSpecies = Set[String]()
+      }
+
+      thisCellSpecies += lsid
+      cellSpecies.put(strRoundedCoords, thisCellSpecies)
+    }
+
+    speciesCellCounts.put(lsid, pointsSet.size)
+  }
+
+  def writeFileOutput(cellSpecies: scala.collection.mutable.Map[String, Set[String]], speciesCellCounts: scala.collection.mutable.Map[String, Int], cellSpeciesFile: String, speciesCellCountsFile: String) {
+    val bwSpeciesCellCounts = new BufferedWriter(new FileWriter(speciesCellCountsFile));
     for (lsid <- speciesCellCounts.keys) {
       bwSpeciesCellCounts.append(lsid)
       bwSpeciesCellCounts.append(",")
@@ -103,7 +144,7 @@ class EndemismLayerHelper {
     bwSpeciesCellCounts.flush()
     bwSpeciesCellCounts.close()
 
-    val bwCellSpecies = new BufferedWriter(new FileWriter(cellSpeciesFilePath));
+    val bwCellSpecies = new BufferedWriter(new FileWriter(cellSpeciesFile));
     for (cellCoords <- cellSpecies.keys) {
       bwCellSpecies.append(cellCoords)
       bwCellSpecies.append(",")
@@ -118,7 +159,7 @@ class EndemismLayerHelper {
 
     var resultsList = new ListBuffer[String]
 
-    def addToList(name: String, count: Int) : Boolean = {
+    def addToList(name: String, count: Int): Boolean = {
       resultsList += name
 
       true
