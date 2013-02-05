91b330a0e9fbc77c1d27a6562985ccb67c0357be
natasha.carter@csiro.au <natasha.carter@csiro.au>
2013-02-05 01:39:29 +0000

    Forgot a file.
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/ala-name-matching@3692 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/java/au/org/ala/checklist/lucene/analyzer/LowerCaseKeywordAnalyzer.java
+++ src/main/java/au/org/ala/checklist/lucene/analyzer/LowerCaseKeywordAnalyzer.java
@@ -1,10 +1,16 @@
 package au.org.ala.checklist.lucene.analyzer;
 
+import au.org.ala.checklist.lucene.NameIndexField;
+import java.io.IOException;
 import java.io.Reader;
+import java.util.Set;
 import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.KeywordTokenizer;
-import org.apache.lucene.analysis.LowerCaseFilter;
+import org.apache.lucene.analysis.core.KeywordTokenizer;
+import org.apache.lucene.analysis.core.LowerCaseFilter;
+import org.apache.lucene.analysis.core.StopFilter;
+import org.apache.lucene.analysis.TokenFilter;
 import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.util.Version;
 
 /**
  * A custom KeywordAnalyzer that converts the text to lowercase before tokenizing
@@ -12,12 +18,44 @@ import org.apache.lucene.analysis.TokenStream;
  *
  * @author Natasha
  */
-public final class LowerCaseKeywordAnalyzer extends Analyzer{
+public final class LowerCaseKeywordAnalyzer extends Analyzer {
 
     @Override
-    public TokenStream tokenStream(String fieldName, Reader reader) {
-         //convert to lowercase
-        return new LowerCaseFilter(new KeywordTokenizer(reader));
+    protected TokenStreamComponents createComponents(String fieldName,
+            Reader reader) {
+            //return new TokenStreamComponents(new KeywordTokenizer(reader));
+
+        KeywordTokenizer src = new KeywordTokenizer(reader);
+        TokenStream result = new LowerCaseFilter(Version.LUCENE_34, src);
+        //KeywordTokenizer src =
+        return new TokenStreamComponents(src, result) {
+
+            @Override
+            protected void setReader(final Reader reader) throws IOException {
+                super.setReader(reader);
+            }
+        };
     }
+
+
+//    @Override
+//    public TokenStream tokenStream(String fieldName, Reader reader) {
+//        //if the field name is VOUCHER then we want to remove fullstops and spaces
+////        if(fieldName.equals(NameIndexField.VOUCHER.toString())){
+////
+////        }
+//         //convert to lowercase
+//        return new LowerCaseFilter(Version.LUCENE_34, new KeywordTokenizer(reader));
+//    }
+//
+//// private final class CharacterRemovalFilter extends TokenFilter {
+////     private CharacterRemovalFilter(TokenStream in) {
+////          super(in);
+////          termAtt = (CharTermAttribute) addAttribute(CharTermAttribute.class);
+////  }
+////
+////  private TermAttribute termAtt;
+////
+//// }
 
 }
