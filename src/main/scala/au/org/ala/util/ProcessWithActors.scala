4f95977a8ca7f09a6b1c41e1f8af7b4235fae01d
natasha.carter@csiro.au <natasha.carter@csiro.au>
2014-02-18 03:00:45 +0000

    Added support for endUuid to be specified
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/biocache-store@4280 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/scala/au/org/ala/util/ProcessWithActors.scala
+++ src/main/scala/au/org/ala/util/ProcessWithActors.scala
@@ -21,18 +21,20 @@ object ProcessWithActors {
     logger.info("Starting processing...")
     var threads:Int = 4
     var startUuid:Option[String] = None
+    var endUuid:Option[String] = None
     var check = false
     var dr:Option[String] = None
 
     val parser = new OptionParser("process records options") {
         intOpt("t", "thread", "The number of threads to use", {v:Int => threads = v } )
         opt("s", "start","The record to start with", {v:String => startUuid = Some(v)})
+        opt("e", "end","The record to end with", {v:String => endUuid = Some(v)})
         opt("dr", "resource", "The data resource to process", {v:String =>dr = Some(v)})
     }
     
     if(parser.parse(args)){
-        logger.info("Processing " + dr.getOrElse("") + " from " + startUuid + " with " + threads + "actors")
-        processRecords(threads, startUuid, dr, check)
+        logger.info("Processing " + dr.getOrElse("") + " from " + startUuid + "to " +endUuid+ " with " + threads + "actors")
+        processRecords(threads, startUuid, dr, check, lastKey = endUuid)
     }
     //shutdown the persistence
     persistenceManager.shutdown
@@ -120,9 +122,9 @@ object ProcessWithActors {
   /**
    * Process the records using the supplied number of threads
    */
-  def processRecords(threads: Int, firstKey:Option[String], dr: Option[String], checkDeleted:Boolean=false, callback:ObserverCallback = null): Unit = {
+  def processRecords(threads: Int, firstKey:Option[String], dr: Option[String], checkDeleted:Boolean=false, callback:ObserverCallback = null, lastKey:Option[String]=None): Unit = {
     
-    val endUuid = if(dr.isEmpty) "" else dr.get +"|~"
+    val endUuid = if (lastKey.isDefined) lastKey.get else if(dr.isEmpty) "" else dr.get +"|~"
     
     val startUuid = {
 	    if(firstKey.isEmpty && !dr.isEmpty) {
