32bc76563d55cf360ca350eaa97dfc7a2f8d3239
natasha.carter@csiro.au <natasha.carter@csiro.au>
2014-03-26 04:48:18 +0000

    Removed dependence to bie-rpc
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/biocache-store@4376 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/scala/au/org/ala/util/TaxonProfileLoader.scala
+++ src/main/scala/au/org/ala/util/TaxonProfileLoader.scala
@@ -1,11 +1,7 @@
 package au.org.ala.util
 
 import au.org.ala.biocache._
-import java.net.InetSocketAddress
-import org.apache.avro.ipc.SocketTransceiver
-import org.apache.avro.specific.SpecificRequestor
-import org.apache.avro.util.Utf8
-import au.org.ala.bie.rpc.{ ProfileArray, Page, SpeciesProfile }
+
 import au.com.bytecode.opencsv.CSVReader
 import java.io.FileReader
 import org.apache.commons.lang.StringUtils
@@ -160,79 +156,7 @@ object TaxonProfileLoader {
       }
   }
     
-    def mainold(args: Array[String]){
 
-    println("Starting the load...")
-    import scala.collection.JavaConversions._
-    
-    val host = args(0)
-    val port = args(1).toInt
-    val client = new SocketTransceiver(new InetSocketAddress(host, port))
-    val proxy = SpecificRequestor.getClient(classOf[SpeciesProfile], client).asInstanceOf[SpeciesProfile]
-    var page = new Page
-    var lastKey = new Utf8("")
-    page.startKey = lastKey
-    page.pageSize = 1000
-    var array = proxy.send(page).asInstanceOf[ProfileArray]
-    var count =0
-
-    //iterate through all the profiles
-    while(array.profiles.size>0){
-	    for (profile <- array.profiles) {
-              count+=1
-              if (count % 1000 == 0 && count > 0) {
-                println(count+">>>" +profile)
-              }
-	      //add to cache
-	      //println(profile)
-	      var taxonProfile = new TaxonProfile
-	      taxonProfile.guid = profile.guid.toString
-	      taxonProfile.scientificName  = profile.scientificName.toString
-	      if(profile.commonName!=null){
-	    	  taxonProfile.commonName = profile.commonName.toString
-	      }
-	      if(profile.habitat!=null){
-	    	  val habitats = for(habitat<-profile.habitat) yield habitat.toString
-	    	  taxonProfile.habitats = habitats.toArray
-	      }
-          if(profile.left != null){
-            taxonProfile.left = profile.left.toString
-          }
-          if(profile.right != null){
-            taxonProfile.right = profile.right.toString
-          }
-          if(profile.rank != null){
-            taxonProfile.rankString = profile.rank.toString
-          }
-          //TODO work out whatto store from the conservation status
-          if(profile.consStatus != null){
-              val css = for(cs<-profile.consStatus)yield{
-                  val region = if(cs.region != null)cs.region.toString else null
-                  val regionId = if(cs.regionId != null) cs.regionId.toString else null
-                  val status = if(cs.status != null) cs.status.toString else null
-                  val rawStatus = if(cs.rawStatus != null) cs.rawStatus.toString else null
-                  
-               new ConservationSpecies(region,regionId, status, rawStatus)}
-              taxonProfile.conservation = css.toArray
-          }
-          //store the sensitive species information
-          //TODO fix up this so that the zone is obtained from the vocabulary?
-          if(profile.sensitiveStatus != null){
-            val sss = for(ss <- profile.sensitiveStatus) 
-              yield new SensitiveSpecies(ss.sensitivityZone.toString, ss.sensitivityCategory.toString)
-              
-            taxonProfile.sensitive = sss.toArray
-          }
-              
-	      TaxonProfileDAO.add(taxonProfile)
-	      lastKey = profile.guid
-	    }
-	    page.startKey = lastKey
-	    array = proxy.send(page).asInstanceOf[ProfileArray]
-    }
-    client.close
-    println("Finished loading " + count + " profiles ")
-  }
   println("Finished species profile loading.")
 }
 
