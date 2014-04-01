0220740106196c88c9a90c5006c51b445ea4b5e6
natasha.carter@csiro.au <natasha.carter@csiro.au>
2014-04-01 04:38:49 +0000

    Fixed issue where MapDataLoader was not substituting DWC values for the key. Also add values for imageLicense to dwc.txt.
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/biocache-store@4404 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/scala/au/org/ala/biocache/load/MapDataLoader.scala
+++ src/main/scala/au/org/ala/biocache/load/MapDataLoader.scala
@@ -3,6 +3,7 @@ package au.org.ala.biocache.load
 import scala.collection.JavaConversions
 import scala.collection.mutable.ArrayBuffer
 import au.org.ala.biocache.model.Versions
+import au.org.ala.biocache.vocab.DwC
 
 /**
  * Created by mar759 on 17/02/2014.
@@ -14,11 +15,16 @@ class MapDataLoader extends DataLoader {
   def load(dataResourceUid:String, values:List[java.util.Map[String,String]], uniqueTerms:List[String]):List[String]={
     val rowKeys = new ArrayBuffer[String]
     values.foreach(jmap =>{
-        val map = jmap.toMap[String,String]
-        val uniqueTermsValues = uniqueTerms.map(t => map.getOrElse(t,""))
-        val fr = FullRecordMapper.createFullRecord("", map, Versions.RAW)
-        load(dataResourceUid, fr, uniqueTermsValues, true, true)
-        rowKeys += fr.rowKey
+      val map = jmap.toMap[String,String]
+      val uniqueTermsValues = uniqueTerms.map(t => map.getOrElse(t,""))
+      //map the keys to DWC values
+      val keys = map.keySet.toList
+      val biocacheModelValues = DwC.retrieveCanonicals(keys)
+      val keysToDwcMap = (keys zip biocacheModelValues).toMap
+      val dwcMap = map.map{case (k,v) => (keysToDwcMap.getOrElse(k,k), v)}
+      val fr = FullRecordMapper.createFullRecord("", dwcMap, Versions.RAW)
+      load(dataResourceUid, fr, uniqueTermsValues, true, true)
+      rowKeys += fr.rowKey
     })
     rowKeys.toList
   }
---
+++ src/test/scala/au/org/ala/biocache/load/MapDataLoaderTest.scala
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2012 Atlas of Living Australia
+ * All Rights Reserved.
+ *
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ */
+package au.org.ala.biocache.load
+
+import au.org.ala.biocache.{Config, ConfigFunSuite}
+import java.util
+
+class MapDataLoaderTest extends ConfigFunSuite {
+  test("map load with dwc substitution"){
+    val loader = new MapDataLoader
+    val map = Map("occurrenceId"->"myid","scientificName"->"Macropus rufus","eventDate"->"2014-04-01","imageLicence"->"CC", "commonName"->"Red Kangaroo")
+    val jmap = new util.HashMap[String,String]()
+    map.foreach{case(k,v)=> jmap.put(k,v)}
+    loader.load("drnq",List(jmap),List("occurrenceId"))
+    println(Config.persistenceManager.get("drnq|myid","occ"))
+    val rights= Config.persistenceManager.get("drnq|myid","occ","rights")
+    expectResult(Some("CC")){rights}
+    expectResult(Some("Red Kangaroo")){Config.persistenceManager.get("drnq|myid","occ","vernacularName")}
+  }
+}
